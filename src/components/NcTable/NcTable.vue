35ed437ebd2da6e4ebe8e10b408599da5f6bc50f
Florian Steffens <florian@nextcloud.com>
2023-01-13 16:25:37 +0100

    refactor(FE table): Skeleton setup for the custom table
    Signed-off-by: Florian Steffens <florian@nextcloud.com>

---
+++ src/components/NcTable/NcTable.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="NcTable">
+		<div class="options row">
+			<Options />
+		</div>
+		<div class="custom-table row">
+			<CustomTable :columns="columns" :rows="rows" />
+		</div>
+	</div>
+</template>
+
+<script>
+import Options from './sections/Options.vue'
+import CustomTable from './sections/CustomTable.vue'
+
+export default {
+	name: 'NcTable',
+	components: { CustomTable, Options },
+	props: {
+		rows: {
+			type: Array,
+			default: () => [],
+		},
+		columns: {
+			type: Array,
+			default: () => [],
+		},
+	},
+}
+</script>
+
+<style scoped>
+
+</style>
