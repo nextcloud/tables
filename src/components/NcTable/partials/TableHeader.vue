d782c7fa3fe22ec32cee315656b46658d8c8b8eb
Florian Steffens <florian@nextcloud.com>
2023-01-13 16:25:37 +0100

    refactor(FE table): Skeleton setup for the custom table
    Signed-off-by: Florian Steffens <florian@nextcloud.com>

---
+++ src/components/NcTable/partials/TableHeader.vue
@@ -0,0 +1,30 @@
+<template>
+	<tr>
+		<th><NcCheckboxRadioSwitch /> </th>
+		<th v-for="col in columns" :key="col.id">
+			{{ col.title }}
+		</th>
+		<th />
+	</tr>
+</template>
+
+<script>
+import { NcCheckboxRadioSwitch } from '@nextcloud/vue'
+
+export default {
+	name: 'TableHeader',
+	components: {
+		NcCheckboxRadioSwitch,
+	},
+	props: {
+		columns: {
+			type: Array,
+			default: () => [],
+		},
+	},
+}
+</script>
+
+<style scoped>
+
+</style>
