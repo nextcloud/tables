ac151f7b222fd4dca5cc4c01c90ef1fa3ffb80aa
Florian Steffens <florian@nextcloud.com>
2023-01-13 16:25:37 +0100

    rebase

---
+++ src/components/NcTable/partials/TableCell.vue
@@ -0,0 +1,29 @@
+<template>
+	<div v-if="value">
+		{{ value }}
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'TableCell',
+	props: {
+		columnId: {
+			type: Number,
+			default: null,
+		},
+		rowId: {
+			type: Number,
+			default: null,
+		},
+		value: {
+			type: String,
+			default: '',
+		},
+	},
+}
+</script>
+
+<style scoped>
+
+</style>
