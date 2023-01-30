eb69b508913226068bf2ec35101830e52de72263
Florian Steffens <florian@nextcloud.com>
2023-01-30 12:05:32 +0100

    fix(table): tables-106 Export "long text" without html in CSV
    Signed-off-by: Florian Steffens <florian@nextcloud.com>

--- src/shared/components/ncTable/mixins/exportTableMixin.js
+++ src/shared/components/ncTable/mixins/exportTableMixin.js
@@ -17,7 +17,14 @@ export default {
 					if (row.data) {
 						set = row.data.find(d => d.columnId === column.id)
 					}
-					rowData[column.title] = set ? set.value : ''
+					let value = ''
+					console.debug('try to export csv', column)
+					if (column.type === 'text' && column.subtype === 'long' && set) {
+						value = set.value.replace(/(<([^>]+)>)/ig, '')
+					} else {
+						value = set ? set.value : ''
+					}
+					rowData[column.title] = value
 				})
 				data.push(rowData)
 			})
@@ -27,9 +34,6 @@ export default {
 			// remove smileys from title
 			const tableTitle = table.title.replace(/([#0-9]\u20E3)|[\xA9\xAE\u203C\u2047-\u2049\u2122\u2139\u3030\u303D\u3297\u3299][\uFE00-\uFEFF]?|[\u2190-\u21FF][\uFE00-\uFEFF]?|[\u2300-\u23FF][\uFE00-\uFEFF]?|[\u2460-\u24FF][\uFE00-\uFEFF]?|[\u25A0-\u25FF][\uFE00-\uFEFF]?|[\u2600-\u27BF][\uFE00-\uFEFF]?|[\u2900-\u297F][\uFE00-\uFEFF]?|[\u2B00-\u2BF0][\uFE00-\uFEFF]?|(?:\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDEFF])[\uFE00-\uFEFF]?/g, '')
 			this.$papa.download(csv, moment().format('YY-MM-DD_HH-mm') + '_' + tableTitle)
-		},
-		getColumnById(colId, columns) {
-			return columns.find(col => col.id === colId)
 		},
 	},
 }
