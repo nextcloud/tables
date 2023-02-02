cb019d506b4ec92d4265af00f8b4a0b48d807de9
Florian Steffens <florian@nextcloud.com>
2023-02-02 12:46:37 +0100

    fix: unused import
    Signed-off-by: Florian Steffens <florian@nextcloud.com>

--- src/App.vue
+++ src/App.vue
@@ -14,8 +14,6 @@
 import { NcContent, NcAppContent } from '@nextcloud/vue'
 import Navigation from './modules/navigation/sections/Navigation.vue'
 import { mapGetters, mapState } from 'vuex'
-import axios from '@nextcloud/axios'
-import { generateOcsUrl } from '@nextcloud/router'
 import Sidebar from './modules/sidebar/sections/Sidebar.vue'
 
 export default {
@@ -46,7 +44,6 @@ export default {
 	},
 	watch: {
 		'$route'(to, from) {
-			// console.debug('route changed', { to, from })
 			this.$store.commit('setActiveTableId', parseInt(to.params.tableId))
 		},
 	},
