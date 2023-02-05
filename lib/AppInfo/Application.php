6064a339d1b6330248524aa0a8fc4dce97b7c18d
Rello <github@scherello.de>
2023-02-05 08:18:07 +0100

    Analytics

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -3,18 +3,31 @@
 namespace OCA\Tables\AppInfo;
 
 use OCA\Tables\Listener\UserDeletedListener;
+use OCA\Tables\Listener\AnalyticsDatasourceListener;
 use OCP\AppFramework\App;
-use OCP\EventDispatcher\IEventDispatcher;
+use OCP\AppFramework\Bootstrap\IBootContext;
+use OCP\AppFramework\Bootstrap\IBootstrap;
+use OCP\AppFramework\Bootstrap\IRegistrationContext;
+
 use OCP\User\Events\BeforeUserDeletedEvent;
+use OCA\Analytics\Datasource\DatasourceEvent;
 
-class Application extends App {
-	public const APP_ID = 'tables';
+class Application extends App implements IBootstrap
+{
+    public const APP_ID = 'tables';
 
-	public function __construct() {
-		parent::__construct(self::APP_ID);
+    public function __construct()
+    {
+        parent::__construct(self::APP_ID);
+    }
 
-		/* @var IEventDispatcher $eventDispatcher */
-		$dispatcher = $this->getContainer()->get(IEventDispatcher::class);
-		$dispatcher->addServiceListener(BeforeUserDeletedEvent::class, UserDeletedListener::class);
-	}
+    public function register(IRegistrationContext $context): void
+    {
+        $context->registerEventListener(BeforeUserDeletedEvent::class, UserDeletedListener::class);
+        $context->registerEventListener(DatasourceEvent::class, AnalyticsDatasourceListener::class);
+    }
+
+    public function boot(IBootContext $context): void
+    {
+    }
 }
