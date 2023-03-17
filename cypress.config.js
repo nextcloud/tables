1e98252c39360dac3cad750dd9d8541a9799289e
sneha <rsneha715@gmail.com>
2023-03-17 12:45:49 +0100

    rebase

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+	baseUrl: 'http://nextcloud.local/index.php/',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
