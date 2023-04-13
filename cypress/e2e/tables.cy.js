24d88cafef24f06a7b695a55f653167ce53582e7
Florian Steffens <florian.steffens@nextcloud.com>
2023-04-13 19:29:52 +0200

    tests(cypress): fix because the template choice changed by a merge
    Signed-off-by: Florian Steffens <florian.steffens@nextcloud.com>

--- cypress/e2e/tables.cy.js
+++ cypress/e2e/tables.cy.js
@@ -1,16 +1,16 @@
-import {User} from "@nextcloud/cypress"
+import { User } from '@nextcloud/cypress'
 describe('The Home Page', () => {
 	it('successfully loads', () => {
 		const user = new User('admin', 'admin')
 		cy.login(user)
 		cy.visit('apps/tables')
 		cy.contains('.app-menu-entry--label', 'Tables').click()
-		cy.contains('button','Create new table').click()
-		cy.get('input[name="template"][value="todo"]').click({force: true})
+		cy.contains('button', 'Create new table').click()
+		cy.get('.tile').contains('ToDo').click({ force: true })
 		cy.get('.modal__content input[type="text"]').type('to do list')
-		cy.contains('button','Create table').click()
-		cy.contains('button','Create row').should('be.visible')
-		cy.contains('h1','to do list').should('be.visible')
+		cy.contains('button', 'Create table').click()
+		cy.contains('button', 'Create row').should('be.visible')
+		cy.contains('h1', 'to do list').should('be.visible')
 		cy.contains('table th', 'Task').should('be.visible')
 	})
 })
