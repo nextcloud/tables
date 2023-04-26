b69edd1951d67267d1c4fb92ce84ecb5472fe81e
Florian Steffens <florian.steffens@nextcloud.com>
2023-04-26 09:11:56 +0200

    refactor fullscreen mode
    Signed-off-by: Florian Steffens <florian.steffens@nextcloud.com>

--- src/shared/components/ncTable/partials/rowTypePartials/TextRichForm.vue
+++ src/shared/components/ncTable/partials/rowTypePartials/TextRichForm.vue
@@ -1,32 +1,31 @@
 <template>
 	<RowFormWrapper :title="column.title" :mandatory="column.mandatory" :description="column.description">
 		<div class="row">
-			<div class="fix-col-4 end space-B-small">
-				<NcButton @click="showBigEditorModal = true">
-					<template #icon>
-						<Fullscreen :size="20" />
-					</template>
-					{{ t('tables', 'Show big editor') }}
-				</NcButton>
-				<NcModal v-if="showBigEditorModal" :close-button-contained="true" size="large" :title="column.title" @close="showBigEditorModal = false">
-					<div class="row">
-						<div class="fix-col-4 space-T-big">
-							<NcEditor :text.sync="localValue" :show-border="false" />
-						</div>
-						<div class="col-4 end space-R space-B">
-							<div class="end">
-								<NcButton @click="showBigEditorModal = false">
-									{{ t('tables', 'Close editor') }}
-								</NcButton>
-							</div>
-						</div>
-					</div>
-				</NcModal>
-			</div>
 			<div class="fix-col-4">
 				<NcEditor v-if="!showBigEditorModal" :text.sync="localValue" height="small" />
 			</div>
 		</div>
+		<template #head>
+			<NcButton type="tertiary" @click="showBigEditorModal = true">
+				<template #icon>
+					<Fullscreen :size="20" />
+				</template>
+			</NcButton>
+			<NcModal v-if="showBigEditorModal" :close-button-contained="true" size="large" :title="column.title" @close="showBigEditorModal = false">
+				<div class="row">
+					<div class="fix-col-4 space-T-big">
+						<NcEditor :text.sync="localValue" :show-border="false" />
+					</div>
+					<div class="col-4 end space-R space-B">
+						<div class="end">
+							<NcButton @click="showBigEditorModal = false">
+								{{ t('tables', 'Close editor') }}
+							</NcButton>
+						</div>
+					</div>
+				</div>
+			</NcModal>
+		</template>
 	</RowFormWrapper>
 </template>
 
