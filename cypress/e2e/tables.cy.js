de50b5741e6c0b3feb33989301e61c70a9e252c5
Florian Steffens <florian.steffens@nextcloud.com>
2023-05-23 11:30:15 +0200

    tests(cypress setup): use random user but for all tests
    Signed-off-by: Florian Steffens <florian.steffens@nextcloud.com>

--- cypress/e2e/tables.cy.js
+++ cypress/e2e/tables.cy.js
@@ -1,14 +1,15 @@
-import { User } from '@nextcloud/cypress'
+let localUser
 
 describe('The Home Page', () => {
 
-	// before(function() {
-	// })
+	before(function() {
+		cy.createRandomUser().then(user => {
+			localUser = user
+		})
+	})
 
 	beforeEach(function() {
-		cy.createRandomUser().then(user => {
-			cy.login(user)
-		})
+		cy.login(localUser)
 		cy.visit('apps/tables')
 	})
 
