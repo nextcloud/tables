e25caf4f62850d1619e7a1e6013581f79dc49f8b
Philipp Hempel <Philipp.Hempel1@web.de>
2023-06-01 11:04:19 +0200

    fix(datecell): Date now is no longer shown as invalid date
    Signed-off-by: Philipp Hempel <Philipp.Hempel1@web.de>

--- src/shared/components/ncTable/partials/TableCellDateTime.vue
+++ src/shared/components/ncTable/partials/TableCellDateTime.vue
@@ -25,8 +25,9 @@ export default {
 	},
 	computed: {
 		getValue() {
+			console.debug(this.value)
 			// default value is for the form, if you want to present the date from today, you should use the calculating column
-			if (!this.value || this.value === 'none' || this.value === 'today') {
+			if (!this.value || this.value === 'none' || this.value === 'today' || this.value === 'now') {
 				return ''
 			}
 
