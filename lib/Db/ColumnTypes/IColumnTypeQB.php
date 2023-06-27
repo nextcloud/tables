14d50337e76cb2f2132b4732910f5d8bd2570926
Philipp Hempel <Philipp.Hempel1@web.de>
2023-06-27 16:08:34 +0200

    Implements backend logic for filtering and extracting the relevant rows
    Signed-off-by: Philipp Hempel <Philipp.Hempel1@web.de>

--- lib/Db/ColumnTypes/IColumnTypeQB.php
+++ lib/Db/ColumnTypes/IColumnTypeQB.php
@@ -2,6 +2,9 @@
 
 namespace OCA\Tables\DB\ColumnTypes;
 
+use OCP\DB\QueryBuilder\IQueryBuilder;
+use OCP\DB\QueryBuilder\IQueryFunction;
+
 interface IColumnTypeQB {
 	public const DB_PLATFORM_MYSQL = 0;
 	public const DB_PLATFORM_PGSQL = 1;
@@ -14,4 +17,32 @@ interface IColumnTypeQB {
 	 * @return mixed
 	 */
 	public function setPlatform(int $platform);
+
+	/**
+	 * @param string $unformattedValue
+	 * @return string
+	 */
+	public function formatCellValue(string $unformattedValue): string;
+
+	/**
+	 * @param IQueryBuilder $qb
+	 * @param string $unformattedSearchValue
+	 * @param string $operator
+	 * @return void
+	 */
+	public function passSearchValue(IQueryBuilder &$qb, string $unformattedSearchValue, string $operator): void;
+
+	/**
+	 * @param IQueryBuilder $qb
+	 * @param array $filter
+	 * @return IQueryFunction
+	 */
+	public function addWhereFilterExpression(IQueryBuilder &$qb, array $filter): IQueryFunction;
+
+	/**
+	 * @param IQueryBuilder $qb
+	 * @param int $columnId
+	 * @return void
+	 */
+	public function addWhereForFindAllWithColumn(IQueryBuilder &$qb, int $columnId): void;
 }
---
+++ lib/Db/ColumnTypes/NumberColumnQB.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace OCA\Tables\Db\ColumnTypes;
+
+use OCA\Tables\Db\View;
+use OCP\DB\QueryBuilder\IQueryBuilder;
+use Psr\Log\LoggerInterface;
+
+class NumberColumnQB extends SuperColumnQB implements IColumnTypeQB {
+	protected LoggerInterface $logger;
+	protected int $platform;
+
+	public function passSearchValue(IQueryBuilder &$qb, string $unformattedSearchValue, string $operator ): void {
+		$qb->setParameter('searchValue', $unformattedSearchValue, $qb::PARAM_INT);
+	}
+}
