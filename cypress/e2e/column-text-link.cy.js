c9b7fef0d15987c3d67f78c735c4808269d143c4
Florian Steffens <florian.steffens@nextcloud.com>
2023-06-28 12:21:13 +0200

    cypress test adjustments
    Signed-off-by: Florian Steffens <florian.steffens@nextcloud.com>

--- cypress/e2e/column-text-link.cy.js
+++ cypress/e2e/column-text-link.cy.js
@@ -31,12 +31,12 @@ describe('Test column text-link', () => {
 		cy.loadTable('Test text-link')
 		cy.get('.NcTable').contains('Create row').click({ force: true })
 		cy.get('.modal__content .slot input').first().type('https://nextcloud.com').tick(500)
-		cy.get('.icon-label-container .labels').contains('https://nextcloud.com').click()
+		cy.get('.icon-label-container .labels a').contains('https://nextcloud.com').click()
 
 		cy.intercept({ method: 'GET', url: '**/search/providers/files/*' }).as('filesResults')
 		cy.get('.modal__content .slot input').eq(1).type('pdf').tick(500)
 		cy.wait('@filesResults')
-		cy.get('.icon-label-container .labels').contains('Nextcloud_Server').click()
+		cy.get('.icon-label-container .labels a').contains('Nextcloud_Server').click()
 
 		cy.get('.modal-container button').contains('Save').click()
 
@@ -52,10 +52,10 @@ describe('Test column text-link', () => {
 		cy.get('.NcTable tr td button').click({ force: true })
 
 		cy.get('.modal__content .slot input').first().clear().type('https://github.com').tick(500)
-		cy.get('.icon-label-container .labels').contains('github').click()
+		cy.get('.icon-label-container .labels a').contains('github').click()
 
 		cy.get('.modal__content .slot input').eq(1).type('photo').tick(500)
-		cy.get('.icon-label-container .labels').contains('photo').click()
+		cy.get('.icon-label-container .labels a').contains('photo').click()
 
 		cy.get('.modal-container button').contains('Save').click()
 
