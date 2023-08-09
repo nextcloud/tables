1907c4c874dd92638286b422e640aa58a0746476
Philipp Hempel <Philipp.Hempel1@web.de>
2023-08-09 12:16:59 +0200

    Small bug fixes
    Signed-off-by: Philipp Hempel <Philipp.Hempel1@web.de>

--- src/shared/components/ncTable/partials/rowTypePartials/RowFormWrapper.vue
+++ src/shared/components/ncTable/partials/rowTypePartials/RowFormWrapper.vue
@@ -6,7 +6,7 @@
 					{{ title }}<span v-if="mandatory" :title="t('tables', 'This field is mandatory')">*</span>
 					<NcLoadingIcon v-if="loading" />
 				</div>
-				<p v-if="description" class="fix-col-4 span">
+				<p v-if="description" class="fix-col-4 span description">
 					{{ description }}
 				</p>
 			</div>
@@ -80,6 +80,10 @@ export default {
 
 .material-design-icon.loading-icon {
 	margin-left: calc(var(--default-grid-baseline) * 1);
+}
+
+.description {
+	white-space: pre-line;
 }
 
 </style>
