1d9fd4ad0a75709d613824f9e9ba659ca0e7f0a4
Julius Härtl <jus@bitgrid.net>
2023-08-10 12:48:04 +0200

    chore: Add cypress project id
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+	projectId: 'ixbf9n',
 	e2e: {
 		baseUrl: 'http://nextcloud.local/index.php/',
 		setupNodeEvents(on, config) {
