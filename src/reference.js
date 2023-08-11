84ad3dd0580147ddd58de0d7e8c2adc9441cb4af
Julius Härtl <jus@bitgrid.net>
2023-08-11 12:21:35 +0200

    chore: Cleanup unused row widget
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- src/reference.js
+++ src/reference.js
@@ -34,20 +34,6 @@ registerWidget('tables_content', async (el, { richObjectType, richObject, access
 	}).$mount(el)
 })
 
-registerWidget('tables_row', async (el, { richObjectType, richObject, accessible }) => {
-	const { default: Vue } = await import(/* webpackChunkName: "reference-table-lazy" */'vue')
-	const { default: TableReferenceWidget } = await import(/* webpackChunkName: "reference-table-lazy" */'./views/RowReferenceWidget.vue')
-	Vue.mixin({ methods: { t, n } })
-	const Widget = Vue.extend(TableReferenceWidget)
-	new Widget({
-		propsData: {
-			richObjectType,
-			richObject,
-			accessible,
-		},
-	}).$mount(el)
-})
-
 registerCustomPickerElement('tables-ref-tables', (el, { providerId, accessible }) => {
 	const Element = Vue.extend(TablesSmartPicker)
 	const vueElement = new Element({
