681dd411e7351f5fed8ef31f12434660f4a2e3fe
Florian Steffens <florian.steffens@nextcloud.com>
2023-08-31 09:41:27 +0200

    fix: fix error for sorting sidebar tabs
    Signed-off-by: Florian Steffens <florian.steffens@nextcloud.com>

--- src/modules/sidebar/sections/Sidebar.vue
+++ src/modules/sidebar/sections/Sidebar.vue
@@ -28,7 +28,7 @@
 			</template>
 			<NcAppSidebarTab
 				id="integration"
-				order="1"
+				:order="1"
 				:name="t('tables', 'Integration')">
 				<SidebarIntegration />
 				<template #icon>
@@ -38,7 +38,7 @@
 			<NcAppSidebarTab v-if="activeElement && canShareElement(activeElement)"
 				id="sharing"
 				icon="icon-share"
-				order="0"
+				:order="0"
 				:name="t('tables', 'Sharing')">
 				<SidebarSharing />
 			</NcAppSidebarTab>
