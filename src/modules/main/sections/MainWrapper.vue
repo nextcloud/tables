f969fc2364b54c27a6c218ae004a86e915e4f7c1
Florian <florian.steffens@nextcloud.com>
2023-09-13 16:02:31 +0200

    fix(design): Fix/545 reset frontend filter on table/view change (#551)
    * fix: reset localViewFilter on every table/view change
    
    Signed-off-by: Florian Steffens <florian.steffens@nextcloud.com>
    
    * test: add test for FE filter info
    - adjust and extend test command
    - add test scenario
    
    Signed-off-by: Florian Steffens <florian.steffens@nextcloud.com>
    
    ---------
    
    Signed-off-by: Florian Steffens <florian.steffens@nextcloud.com>

--- src/modules/main/sections/MainWrapper.vue
+++ src/modules/main/sections/MainWrapper.vue
@@ -110,7 +110,7 @@ export default {
 			if (!this.lastActiveElement || this.element.id !== this.lastActiveElement.id || this.isView !== this.lastActiveElement.isView || force) {
 				this.localLoading = true
 
-				if (this.isView) this.viewSetting = {}
+				this.viewSetting = {}
 
 				await this.$store.dispatch('loadColumnsFromBE', {
 					view: this.isView ? this.element : null,
