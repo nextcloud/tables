bc2e4f90e17937158b5b20982473f3d2c9ef72b0
Rello <Rello@users.noreply.github.com>
2023-09-20 13:39:53 +0200

    Update V1Api.php
    Signed-off-by: Rello <Rello@users.noreply.github.com>

--- lib/Api/V1Api.php
+++ lib/Api/V1Api.php
@@ -3,59 +3,76 @@
 namespace OCA\Tables\Api;
 
 use OCA\Tables\Errors\InternalError;
+use OCA\Tables\Errors\NotFoundError;
 use OCA\Tables\Errors\PermissionError;
 use OCA\Tables\Service\ColumnService;
 use OCA\Tables\Service\RowService;
+use OCP\AppFramework\Db\DoesNotExistException;
+use OCP\AppFramework\Db\MultipleObjectsReturnedException;
 
-class V1Api {
-	private RowService $rowService;
-	private ColumnService $columnService;
-	private ?string $userId;
+class V1Api
+{
+    private RowService $rowService;
+    private ColumnService $columnService;
+    private ?string $userId;
 
-	public function __construct(ColumnService $columnService, RowService $rowService, ?string $userId) {
-		$this->columnService = $columnService;
-		$this->rowService = $rowService;
-		$this->userId = $userId;
-	}
+    public function __construct(ColumnService $columnService, RowService $rowService, ?string $userId)
+    {
+        $this->columnService = $columnService;
+        $this->rowService = $rowService;
+        $this->userId = $userId;
+    }
 
-	/**
-	 * @param int $tableId
-	 * @param int|null $limit
-	 * @param int|null $offset
-	 * @return array
-	 * @throws InternalError
-	 * @throws PermissionError
-	 */
-	public function getData(int $tableId, ?int $limit, ?int $offset): array {
-		$columns = $this->columnService->findAllByTable($tableId);
+    /**
+     * @param int $tableId
+     * @param int|null $limit
+     * @param int|null $offset
+     * @param int|null $userId
+     * @param string|null $nodeType
+     * @return array
+     * @throws DoesNotExistException
+     * @throws InternalError
+     * @throws MultipleObjectsReturnedException
+     * @throws NotFoundError
+     * @throws PermissionError
+     */
+    public function getData(int $tableId, ?int $limit, ?int $offset, ?int $userId, ?string $nodeType = null): array
+    {
+        if ($userId) $this->userId = $userId;
+        if ($nodeType === 'view') {
+            $columns = $this->columnService->findAllByView($tableId, $this->userId);
+            $rows = $this->rowService->findAllByView($tableId, $this->userId, $limit, $offset);
+        } else {
+            // if no nodeType is provided, the old table selection is used to not break anything
+            $columns = $this->columnService->findAllByTable($tableId, null, $this->userId);
+            $rows = $this->rowService->findAllByTable($tableId, $this->userId, $limit, $offset);
+        }
 
-		$rows = $this->rowService->findAllByTable($tableId, $this->userId, $limit, $offset);
+        $data = [];
 
-		$data = [];
+        // first line contains the titles
+        $header = [];
+        foreach ($columns as $column) {
+            $header[] = $column->getTitle();
+        }
+        $data[] = $header;
 
-		// first line contains the titles
-		$header = [];
-		foreach ($columns as $column) {
-			$header[] = $column->getTitle();
-		}
-		$data[] = $header;
+        // now add the rows
+        foreach ($rows as $row) {
+            $rowData = $row->getDataArray();
+            $line = [];
+            foreach ($columns as $column) {
+                $value = '';
+                foreach ($rowData as $datum) {
+                    if ($datum['columnId'] === $column->getId()) {
+                        $value = $datum['value'];
+                    }
+                }
+                $line[] = $value;
+            }
+            $data[] = $line;
+        }
 
-		// now add the rows
-		foreach ($rows as $row) {
-			$rowData = $row->getDataArray();
-			$line = [];
-			foreach ($columns as $column) {
-				$value = '';
-				foreach ($rowData as $datum) {
-					if ($datum['columnId'] === $column->getId()) {
-						$value = $datum['value'];
-					}
-				}
-				$line[] = $value;
-			}
-			$data[] = $line;
-		}
-
-		return $data;
-	}
+        return $data;
+    }
 }
