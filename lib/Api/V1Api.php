680fe4c84e3ee97385488e0e63c8c2d7f566bdea
Florian Steffens <florian.steffens@nextcloud.com>
2023-10-30 08:11:06 +0100

    linting
    Signed-off-by: Florian Steffens <florian.steffens@nextcloud.com>

--- lib/Api/V1Api.php
+++ lib/Api/V1Api.php
@@ -10,69 +10,68 @@ use OCA\Tables\Service\RowService;
 use OCP\AppFramework\Db\DoesNotExistException;
 use OCP\AppFramework\Db\MultipleObjectsReturnedException;
 
-class V1Api
-{
-    private RowService $rowService;
-    private ColumnService $columnService;
-    private ?string $userId;
+class V1Api {
+	private RowService $rowService;
+	private ColumnService $columnService;
+	private ?string $userId;
 
-    public function __construct(ColumnService $columnService, RowService $rowService, ?string $userId)
-    {
-        $this->columnService = $columnService;
-        $this->rowService = $rowService;
-        $this->userId = $userId;
-    }
+	public function __construct(ColumnService $columnService, RowService $rowService, ?string $userId) {
+		$this->columnService = $columnService;
+		$this->rowService = $rowService;
+		$this->userId = $userId;
+	}
 
-    /**
-     * @param int $tableId
-     * @param int|null $limit
-     * @param int|null $offset
-     * @param string|null $userId
-     * @param string|null $nodeType
-     * @return array
-     * @throws DoesNotExistException
-     * @throws InternalError
-     * @throws MultipleObjectsReturnedException
-     * @throws NotFoundError
-     * @throws PermissionError
-     */
-    public function getData(int $tableId, ?int $limit, ?int $offset, ?string $userId, ?string $nodeType = null): array
-    {
-        if ($userId) $this->userId = $userId;
-        if ($nodeType === 'view') {
-            $columns = $this->columnService->findAllByView($tableId, $this->userId);
-            $rows = $this->rowService->findAllByView($tableId, $this->userId, $limit, $offset);
-        } else {
-            // if no nodeType is provided, the old table selection is used to not break anything
-            $columns = $this->columnService->findAllByTable($tableId, null, $this->userId);
-            $rows = $this->rowService->findAllByTable($tableId, $this->userId, $limit, $offset);
-        }
+	/**
+	 * @param int $tableId
+	 * @param int|null $limit
+	 * @param int|null $offset
+	 * @param string|null $userId
+	 * @param string|null $nodeType
+	 * @return array
+	 * @throws DoesNotExistException
+	 * @throws InternalError
+	 * @throws MultipleObjectsReturnedException
+	 * @throws NotFoundError
+	 * @throws PermissionError
+	 */
+	public function getData(int $tableId, ?int $limit, ?int $offset, ?string $userId, ?string $nodeType = null): array {
+		if ($userId) {
+			$this->userId = $userId;
+		}
+		if ($nodeType === 'view') {
+			$columns = $this->columnService->findAllByView($tableId, $this->userId);
+			$rows = $this->rowService->findAllByView($tableId, $this->userId, $limit, $offset);
+		} else {
+			// if no nodeType is provided, the old table selection is used to not break anything
+			$columns = $this->columnService->findAllByTable($tableId, null, $this->userId);
+			$rows = $this->rowService->findAllByTable($tableId, $this->userId, $limit, $offset);
+		}
 
-        $data = [];
+		$data = [];
 
-        // first line contains the titles
-        $header = [];
-        foreach ($columns as $column) {
-            $header[] = $column->getTitle();
-        }
-        $data[] = $header;
+		// first line contains the titles
+		$header = [];
+		foreach ($columns as $column) {
+			$header[] = $column->getTitle();
+		}
+		$data[] = $header;
 
-        // now add the rows
-        foreach ($rows as $row) {
-            $rowData = $row->getDataArray();
-            $line = [];
-            foreach ($columns as $column) {
-                $value = '';
-                foreach ($rowData as $datum) {
-                    if ($datum['columnId'] === $column->getId()) {
-                        $value = $datum['value'];
-                    }
-                }
-                $line[] = $value;
-            }
-            $data[] = $line;
-        }
+		// now add the rows
+		foreach ($rows as $row) {
+			$rowData = $row->getDataArray();
+			$line = [];
+			foreach ($columns as $column) {
+				$value = '';
+				foreach ($rowData as $datum) {
+					if ($datum['columnId'] === $column->getId()) {
+						$value = $datum['value'];
+					}
+				}
+				$line[] = $value;
+			}
+			$data[] = $line;
+		}
 
-        return $data;
-    }
+		return $data;
+	}
 }
--- lib/Datasource/AnalyticsDatasource.php
+++ lib/Datasource/AnalyticsDatasource.php
@@ -23,187 +23,180 @@ use OCP\AppFramework\Db\MultipleObjectsReturnedException;
 use OCP\IL10N;
 use Psr\Log\LoggerInterface;
 
-class AnalyticsDatasource implements IDatasource
-{
-    private LoggerInterface $logger;
-    private IL10N $l10n;
-    private TableService $tableService;
-    private ViewService $viewService;
-    private V1Api $api;
+class AnalyticsDatasource implements IDatasource {
+	private LoggerInterface $logger;
+	private IL10N $l10n;
+	private TableService $tableService;
+	private ViewService $viewService;
+	private V1Api $api;
 
-    protected ?string $userId;
+	protected ?string $userId;
 
-    public function __construct(
-        IL10N           $l10n,
-        LoggerInterface $logger,
-        TableService    $tableService,
-        ViewService     $viewService,
-        V1Api           $api,
-        ?string         $userId
-    )
-    {
-        $this->l10n = $l10n;
-        $this->logger = $logger;
-        $this->tableService = $tableService;
-        $this->viewService = $viewService;
-        $this->api = $api;
-        $this->userId = $userId;
-    }
+	public function __construct(
+		IL10N           $l10n,
+		LoggerInterface $logger,
+		TableService    $tableService,
+		ViewService     $viewService,
+		V1Api           $api,
+		?string         $userId
+	) {
+		$this->l10n = $l10n;
+		$this->logger = $logger;
+		$this->tableService = $tableService;
+		$this->viewService = $viewService;
+		$this->api = $api;
+		$this->userId = $userId;
+	}
 
-    /**
-     * @return string Display Name of the datasource
-     */
-    public function getName(): string
-    {
-        return $this->l10n->t('Nextcloud Tables');
-    }
+	/**
+	 * @return string Display Name of the datasource
+	 */
+	public function getName(): string {
+		return $this->l10n->t('Nextcloud Tables');
+	}
 
-    /**
-     * @return int 2 digit unique datasource id
-     */
-    public function getId(): int
-    {
-        return 55;
-    }
+	/**
+	 * @return int 2 digit unique datasource id
+	 */
+	public function getId(): int {
+		return 55;
+	}
 
-    /**
-     * available options of the data source
-     *
-     * return needs to be an array and can consist of many fields.
-     * every field needs to provide the following format
-     *  id          *mandatory*     = name of the option for the readData
-     *  name        *mandatory*     = displayed name of the input field in the UI
-     *  type        *optional*      = 'tf' to create a dropdown. Values need to be provided in the placeholder separated with "/".
-     *  placeholder *mandatory*     = help text for the input field in the UI
-     *                                for type=tf:
-     *                                  e.g. "true/false"
-     *                                  if value/text pairs are required for the dropdown/option, the values need to be separated with "-" in addition.
-     *                                  e.g. "eq-equal/gt-greater"
-     *                                  to avoid translation of the technical strings, separate them
-     *                                  'true-' - $this->l10n->t('Yes').'/false-'.$this->l10n->t('No')
-     *
-     *  example:
-     *  {['id' => 'datatype', 'name' => 'Type of Data', 'type' => 'tf', 'placeholder' => 'adaptation/absolute']}
-     *
-     * @return array
-     * @throws DoesNotExistException
-     * @throws InternalError
-     * @throws MultipleObjectsReturnedException
-     * @throws NotFoundError
-     * @throws PermissionError
-     */
-    public function getTemplate(): array
-    {
-        $tableString = '';
-        $template = [];
-        $tables = [];
+	/**
+	 * available options of the data source
+	 *
+	 * return needs to be an array and can consist of many fields.
+	 * every field needs to provide the following format
+	 *  id          *mandatory*     = name of the option for the readData
+	 *  name        *mandatory*     = displayed name of the input field in the UI
+	 *  type        *optional*      = 'tf' to create a dropdown. Values need to be provided in the placeholder separated with "/".
+	 *  placeholder *mandatory*     = help text for the input field in the UI
+	 *                                for type=tf:
+	 *                                  e.g. "true/false"
+	 *                                  if value/text pairs are required for the dropdown/option, the values need to be separated with "-" in addition.
+	 *                                  e.g. "eq-equal/gt-greater"
+	 *                                  to avoid translation of the technical strings, separate them
+	 *                                  'true-' - $this->l10n->t('Yes').'/false-'.$this->l10n->t('No')
+	 *
+	 *  example:
+	 *  {['id' => 'datatype', 'name' => 'Type of Data', 'type' => 'tf', 'placeholder' => 'adaptation/absolute']}
+	 *
+	 * @return array
+	 * @throws DoesNotExistException
+	 * @throws InternalError
+	 * @throws MultipleObjectsReturnedException
+	 * @throws NotFoundError
+	 * @throws PermissionError
+	 */
+	public function getTemplate(): array {
+		$tableString = '';
+		$template = [];
+		$tables = [];
 
-        // get all tables for the user
-        if ($this->userId) {
-            $tables = $this->tableService->findAll($this->userId);
-        }
+		// get all tables for the user
+		if ($this->userId) {
+			$tables = $this->tableService->findAll($this->userId);
+		}
 
-        // concatenate the option-string. The format is tableId:viewId-title
-        foreach ($tables as $table) {
-            $tableString = $tableString . $table->getId() . '-' . $table->getTitle() . '/';
-            // get all views per table
-            $views = $this->viewService->findAll($this->tableService->find($table->getId()));
-            foreach ($views as $view) {
-                $tableString = $tableString . $table->getId() . ':' . $view->getId() . '-' . $view->getTitle() . '/';
-            }
-        }
+		// concatenate the option-string. The format is tableId:viewId-title
+		foreach ($tables as $table) {
+			$tableString = $tableString . $table->getId() . '-' . $table->getTitle() . '/';
+			// get all views per table
+			$views = $this->viewService->findAll($this->tableService->find($table->getId()));
+			foreach ($views as $view) {
+				$tableString = $tableString . $table->getId() . ':' . $view->getId() . '-' . $view->getTitle() . '/';
+			}
+		}
 
-        // add the tables to a dropdown in the data source settings
-        $template[] = ['id' => 'tableId', 'name' => $this->l10n->t('Select table'), 'type' => 'tf', 'placeholder' => $tableString];
-        $template[] = ['id' => 'columns', 'name' => $this->l10n->t('Select columns'), 'placeholder' => $this->l10n->t('e.g. 1,2,4 or leave empty'), 'type' => 'columnPicker'];
-        $template[] = ['id' => 'timestamp', 'name' => $this->l10n->t('Timestamp of data load'), 'placeholder' => 'false-' . $this->l10n->t('No') . '/true-' . $this->l10n->t('Yes'), 'type' => 'tf'];
-        return $template;
-    }
+		// add the tables to a dropdown in the data source settings
+		$template[] = ['id' => 'tableId', 'name' => $this->l10n->t('Select table'), 'type' => 'tf', 'placeholder' => $tableString];
+		$template[] = ['id' => 'columns', 'name' => $this->l10n->t('Select columns'), 'placeholder' => $this->l10n->t('e.g. 1,2,4 or leave empty'), 'type' => 'columnPicker'];
+		$template[] = ['id' => 'timestamp', 'name' => $this->l10n->t('Timestamp of data load'), 'placeholder' => 'false-' . $this->l10n->t('No') . '/true-' . $this->l10n->t('Yes'), 'type' => 'tf'];
+		return $template;
+	}
 
-    /**
-     * Read the Data
-     *
-     * return needs to be an array
-     *  [
-     *      'header' => $header,  //array('column header 1', 'column header 2','column header 3')
-     *      'dimensions' => array_slice($header, 0, count($header) - 1),
-     *      'data' => $data,
-     *      'error' => 0,         // INT 0 = no error
-     *  ]
-     *
-     * @param array $option
-     * @return array available options of the data source
-     * @throws InternalError
-     * @throws PermissionError
-     * @throws NotFoundError
-     * @throws DoesNotExistException
-     * @throws MultipleObjectsReturnedException
-     */
-    public function readData($option): array
-    {
-        // get the ids which come in the format tableId:viewId
-        $ids = explode(':', $option['tableId']);
-        $this->userId = $option['user_id'];
+	/**
+	 * Read the Data
+	 *
+	 * return needs to be an array
+	 *  [
+	 *      'header' => $header,  //array('column header 1', 'column header 2','column header 3')
+	 *      'dimensions' => array_slice($header, 0, count($header) - 1),
+	 *      'data' => $data,
+	 *      'error' => 0,         // INT 0 = no error
+	 *  ]
+	 *
+	 * @param array $option
+	 * @return array available options of the data source
+	 * @throws InternalError
+	 * @throws PermissionError
+	 * @throws NotFoundError
+	 * @throws DoesNotExistException
+	 * @throws MultipleObjectsReturnedException
+	 */
+	public function readData($option): array {
+		// get the ids which come in the format tableId:viewId
+		$ids = explode(':', $option['tableId']);
+		$this->userId = $option['user_id'];
 
-        if (count($ids) === 1) {
-            // its a table
-            $data = $this->api->getData($ids[0], null, null, $this->userId);
-        } elseif (count($ids) === 2) {
-            // its a view
-            $data = $this->api->getData($ids[1], null, null, $this->userId, 'view');
-        }
+		if (count($ids) === 1) {
+			// its a table
+			$data = $this->api->getData($ids[0], null, null, $this->userId);
+		} elseif (count($ids) === 2) {
+			// its a view
+			$data = $this->api->getData($ids[1], null, null, $this->userId, 'view');
+		}
 
-        // extract the header from the first row
-        $header = $data[0];
-        // continue with the data without header
-        $rows = array_slice($data, 1);
+		// extract the header from the first row
+		$header = $data[0];
+		// continue with the data without header
+		$rows = array_slice($data, 1);
 
-        // get the selected columns from the data source options
-        $selectedColumns = [];
-        if (isset($option['columns']) && strlen($option['columns']) > 0) {
-            $selectedColumns = str_getcsv($option['columns']);
-        }
+		// get the selected columns from the data source options
+		$selectedColumns = [];
+		if (isset($option['columns']) && strlen($option['columns']) > 0) {
+			$selectedColumns = str_getcsv($option['columns']);
+		}
 
-        $data = [];
-        if (count($selectedColumns) !== 0) {
-            // if dedicated columns are selected, the others are removed
-            $header = $this->minimizeRow($selectedColumns, $header);
-            foreach ($rows as $row) {
-                $data[] = $this->minimizeRow($selectedColumns, $row);
-            }
-        } else {
-            foreach ($rows as $row) {
-                $data[] = $row;
-            }
-        }
-        unset($rows);
+		$data = [];
+		if (count($selectedColumns) !== 0) {
+			// if dedicated columns are selected, the others are removed
+			$header = $this->minimizeRow($selectedColumns, $header);
+			foreach ($rows as $row) {
+				$data[] = $this->minimizeRow($selectedColumns, $row);
+			}
+		} else {
+			foreach ($rows as $row) {
+				$data[] = $row;
+			}
+		}
+		unset($rows);
 
-        return [
-            'header' => $header,
-            'dimensions' => array_slice($header, 0, count($header) - 1),
-            'data' => $data,
-            //'rawdata' => $data,
-            'error' => 0,
-        ];
-    }
+		return [
+			'header' => $header,
+			'dimensions' => array_slice($header, 0, count($header) - 1),
+			'data' => $data,
+			//'rawdata' => $data,
+			'error' => 0,
+		];
+	}
 
-    /**
-     * filter only the selected columns in the given sequence
-     *
-     * @param array $selectedColumns
-     * @param array $row
-     * @return array
-     */
-    private function minimizeRow(array $selectedColumns, array $row): array
-    {
-        $rowMinimized = [];
-        foreach ($selectedColumns as $selectedColumn) {
-            if ((int)$selectedColumn) {
-                $rowMinimized[] = $row[$selectedColumn - 1];
-            } else {
-                $rowMinimized[] = $selectedColumn;
-            }
-        }
-        return $rowMinimized;
-    }
+	/**
+	 * filter only the selected columns in the given sequence
+	 *
+	 * @param array $selectedColumns
+	 * @param array $row
+	 * @return array
+	 */
+	private function minimizeRow(array $selectedColumns, array $row): array {
+		$rowMinimized = [];
+		foreach ($selectedColumns as $selectedColumn) {
+			if ((int)$selectedColumn) {
+				$rowMinimized[] = $row[$selectedColumn - 1];
+			} else {
+				$rowMinimized[] = $selectedColumn;
+			}
+		}
+		return $rowMinimized;
+	}
 }
