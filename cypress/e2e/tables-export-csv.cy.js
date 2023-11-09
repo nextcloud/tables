b2fbc67e0f2543d4da08128fd60d639d1fe8245c
Florian Steffens <florian.steffens@nextcloud.com>
2023-11-09 09:34:57 +0100

    fix(test): add "0" if hours or minutes are just 1 character to meet the file names correctly
    Signed-off-by: Florian Steffens <florian.steffens@nextcloud.com>

--- cypress/e2e/tables-export-csv.cy.js
+++ cypress/e2e/tables-export-csv.cy.js
@@ -16,8 +16,10 @@ describe('Import csv', () => {
 	it('Export csv', () => {
 		cy.loadTable('Tutorial')
 		cy.clickOnTableThreeDotMenu('Export as CSV')
-		const time = new Date()
-		const fileName = new Date().toISOString().slice(2, 10) + '_' + time.getHours() + '-' + time.getMinutes() + '_' + 'Tutorial.csv'
+		const hour = new Date().getHours().toString().length < 2 ? '0' + new Date().getHours() : new Date().getHours().toString()
+		const minutes = new Date().getMinutes().toString().length < 2 ? '0' + new Date().getMinutes() : new Date().getMinutes().toString()
+		const date = new Date().toISOString().slice(2, 10)
+		const fileName = date + '_' + hour + '-' + minutes + '_' + 'Tutorial.csv'
 		cy.log('filename: ' + fileName)
 		cy.readFile('cypress/downloads/' + fileName).should('contain', 'What,How to do,Ease of use,Done')
 		cy.readFile('cypress/downloads/' + fileName).should('contain', 'Open the tables app,Click on tables icon in the menu bar.,5,true')
