b96a33a4ad808cffa4fbeab93274ade7b5112c22
Florian Steffens <florian.steffens@nextcloud.com>
2023-12-19 12:03:54 +0100

    update rowService to use Row2 everywhere - adjust RowSleeveMapper.php for it
    Signed-off-by: Florian Steffens <florian.steffens@nextcloud.com>

--- lib/Db/RowSleeveMapper.php
+++ lib/Db/RowSleeveMapper.php
@@ -8,13 +8,16 @@ use OCP\AppFramework\Db\QBMapper;
 use OCP\DB\Exception;
 use OCP\DB\QueryBuilder\IQueryBuilder;
 use OCP\IDBConnection;
+use Psr\Log\LoggerInterface;
 
 /** @template-extends QBMapper<RowSleeve> */
 class RowSleeveMapper extends QBMapper {
 	protected string $table = 'tables_row_sleeves';
+	protected LoggerInterface $logger;
 
-	public function __construct(IDBConnection $db) {
+	public function __construct(IDBConnection $db, LoggerInterface $logger) {
 		parent::__construct($db, $this->table, RowSleeve::class);
+		$this->logger = $logger;
 	}
 
 	/**
@@ -54,5 +57,40 @@ class RowSleeveMapper extends QBMapper {
 				$qb->expr()->eq('id', $qb->createNamedParameter($sleeveId, IQueryBuilder::PARAM_INT))
 			);
 		$qb->executeStatement();
+	}
+
+	/**
+	 * @param int $tableId
+	 * @return int Effected rows
+	 * @throws Exception
+	 */
+	public function deleteAllForTable(int $tableId): int {
+		$qb = $this->db->getQueryBuilder();
+		$qb->delete($this->tableName)
+			->where(
+				$qb->expr()->eq('table_id', $qb->createNamedParameter($tableId, IQueryBuilder::PARAM_INT))
+			);
+		return $qb->executeStatement();
+	}
+
+	/**
+	 * @param int $tableId
+	 * @return int
+	 */
+	public function countRows(int $tableId): int {
+		$qb = $this->db->getQueryBuilder();
+		$qb->select($qb->func()->count('*', 'counter'));
+		$qb->from($this->table, 't1');
+		$qb->where(
+			$qb->expr()->eq('table_id', $qb->createNamedParameter($tableId))
+		);
+
+		try {
+			$result = $this->findOneQuery($qb);
+			return (int)$result['counter'];
+		} catch (DoesNotExistException|MultipleObjectsReturnedException|Exception $e) {
+			$this->logger->warning('Exception occurred: '.$e->getMessage().' Will return 0.');
+			return 0;
+		}
 	}
 }
