a115f9d2c937d6f6f61ef917892bedd2c7044804
Julius Härtl <jus@bitgrid.net>
2024-01-05 10:31:45 +0100

    chore: move to cypress action
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,34 @@
 const { defineConfig } = require('cypress')
+const { configureNextcloud, startNextcloud, stopNextcloud, waitOnNextcloud } = require('@nextcloud/cypress/docker')
 
 module.exports = defineConfig({
 	projectId: 'ixbf9n',
 	e2e: {
 		baseUrl: 'http://nextcloud.local/index.php/',
 		setupNodeEvents(on, config) {
-			// implement node event listeners here
+			if (process.env.CYPRESS_baseUrl) {
+				return config
+			}
+			// Remove container after run
+			on('after:run', () => {
+				if (!process.env.CI) {
+					stopNextcloud()
+				}
+			})
+
+			// starting Nextcloud testing container with specified server branch
+			return startNextcloud(process.env.BRANCH)
+				.then((ip) => {
+					// Setting container's IP as base Url
+					config.baseUrl = `http://${ip}/index.php`
+					return ip
+				})
+				.then(waitOnNextcloud)
+				// configure Nextcloud, also enable the app
+				.then(() => configureNextcloud(['tables']))
+				.then(() => {
+					return config
+				})
 		},
 	  pageLoadTimeout: 120000,
 	},
