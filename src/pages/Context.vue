2066a61ec4ea796951fc5ebdce20dcce5714e7b5
Cleopatra Enjeck M <patrathewhiz@gmail.com>
2024-02-19 12:40:37 +0100

    update
    Signed-off-by: Cleopatra Enjeck M <patrathewhiz@gmail.com>

--- src/pages/Context.vue
+++ src/pages/Context.vue
@@ -1,123 +1,108 @@
 <template>
-	<div v-if="dataReady">
-		<div v-for="id in ids">
-			<NcTable 
-            :key="id" 
-            :columns="tableData['columns'+id]" 
-            :rows="tableData['rows'+id]" 
-            :element-id="id"
-            :is-view="false"
-            :can-read-rows="true"
-            :can-create-rows="true"
-            :can-edit-rows="true"
-            :can-delete-rows="true"
-            :can-create-columns="true"
-            :can-edit-columns="true"
-            :can-delete-columns="true"
-            :can-delete-table="true"
-            :can-select-rows="true"
-            :can-hide-rows="true"
-            :can-filter="true"
-            :show-actions="true"
-            @import="openImportModal"
-            @create-column="createColumn"
-            @edit-column="editColumn"
-            @delete-column="deleteColumn"
-            @create-row="createRow"
-            @edit-row="editRow"
-            @delete-selected-rows="deleteSelectedRows"
-            />
+	<div>
+		<div v-if="dataReady">
+			<div v-for="table in tables">
+				<TableWrapper :table="table" :columns="tableData['columns' + table.id]"
+					:rows="tableData['rows' + table.id]" :view-setting="viewSetting"
+					@create-column="createColumn(false, table)"
+					@import="openImportModal(table, false)" @download-csv="downloadCSV(table, false)" />
+			</div>
+			<div v-for="view in views">
+				<CustomView :view="view"
+					:columns="tableData['view-columns' + view.id]" :rows="tableData['view-rows' + view.id]" :view-setting="viewSetting"
+					@create-column="createColumn(true, view)"
+					@import="openImportModal(view, true)" @download-csv="downloadCSV(view, true)" />
+			</div>
+			<MainModals />
 		</div>
-        <MainModals />
 	</div>
 </template>
 
 <script>
-import NcTable from '../shared/components/ncTable/NcTable.vue'
 import MainModals from '../modules/modals/Modals.vue'
-import Vuex from 'vuex'
+import Vuex, { mapState } from 'vuex'
 import Vue from 'vue'
+import CustomView from '../modules/main/sections/View.vue'
+import TableWrapper from '../modules/main/sections/TableWrapper.vue'
 import { emit } from '@nextcloud/event-bus'
 
 Vue.use(Vuex)
 
 export default {
 	components: {
-		NcTable,
 		MainModals,
+		CustomView,
+		TableWrapper,
 	},
 
 	data() {
 		return {
 			dataReady: false,
-			ids: [1, 2, 3], //replace with existing table ids
+			viewSetting: {},
 		}
 	},
 
 	computed: {
+		...mapState(['tables', 'views']),
 		tableData() {
 			const data = {}
-			this.ids.forEach((id) => {
-                const rowId = "rows"+id
-                const colId = "columns"+id
-                data[colId] = this.$store.state.data.columns[id]
-                data[rowId] = this.$store.state.data.rows[id]
+			this.tables.forEach((table, index) => {
+				const rowId = 'rows' + table.id
+				const colId = 'columns' + table.id
+				data[colId] = this.$store.state.data.columns[(table.id).toString()]
+				data[rowId] = this.$store.state.data.rows[(table.id).toString()]
+			})
+			this.views.forEach((view, index) => {
+				const rowId = 'view-rows' + view.id
+				const colId = 'view-columns' + view.id
+				data[colId] = this.$store.state.data.columns['view-' + view.id]
+				data[rowId] = this.$store.state.data.rows['view-' + view.id]
 			})
 			return data
 		},
 	},
 
 	async beforeMount() {
-		for (const id of this.ids) {
+		for (const table of this.tables) {
 			await this.$store.dispatch('loadColumnsFromBE', {
-				viewId: null,
-				tableId: id,
+				view: null,
+				tableId: table.id,
 			})
 			await this.$store.dispatch('loadRowsFromBE', {
 				viewId: null,
-				tableId: id,
+				tableId: table.id,
 			})
 		}
+
+		for (const view of this.views) {
+			await this.$store.dispatch('loadColumnsFromBE', {
+				view,
+				tableId: null,
+			})
+			await this.$store.dispatch('loadRowsFromBE', {
+				viewId: view.id,
+				tableId: null,
+			})
+		}
+
 		this.dataReady = true
 	},
 
 	methods: {
-        createColumn() {
-			// emit('tables:column:create', { isView: this.isView, element: this.element })
+		createColumn(isView, element) {
+			emit('tables:column:create', { isView, element })
 		},
-		editColumn(column, elementId, isView) {
-			emit('tables:column:edit', { column, isView, elementId })
+		downloadCSV(element, isView) {
+			const rowId = !isView ? 'rows' + element.id : 'view-rows' + element.id
+			const colId = !isView ? 'columns' + element.id : 'view-columns' + element.id
+			this.downloadCsv(this.tableData[rowId], this.tableData[colId], element.title)
 		},
-		deleteColumn(column, elementId, isView) {
-			emit('tables:column:delete', { column, isView, elementId })
-		},
-		createRow( elementId, isView) {
-            const colId = "columns"+elementId
-			emit('tables:row:create', {columns: this.tableData[colId], isView, elementId})
-		},
-		editRow(rowId, elementId, isView) {
-            // const colId = "columns"+elementId
-            // const rowIdState = "rows"+elementId
-			// emit('tables:row:edit', { row: this.tableData[rowIdState].find(r => r.id === rowId), columns: this.tableData[colId] })
-		},
-		deleteSelectedRows(rows, elementId, isView) {
-			// emit('tables:row:delete', rows, isView, elementId)
-		},
-
-		toggleShare(tableId) {
-			// emit('tables:sidebar:sharing', { open: true, tab: 'sharing' })
-		},
-		actionShowIntegration() {
-			// emit('tables:sidebar:integration', { open: true, tab: 'integration' })
-		},
-		openImportModal(element) {
-			// emit('tables:modal:import', { element, isView: false })
+		openImportModal(element, isView) {
+			emit('tables:modal:import', { element, isView })
 		},
 	},
 }
 
 </script>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
