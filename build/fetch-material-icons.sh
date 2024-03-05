c8234a09e12da8fa91ca465ed6aa20c2f1e11db1
Arthur Schiwon <blizzz@arthur-schiwon.de>
2024-03-05 16:41:47 +0100

    fix(icons): icons are expected to be white by default for theming
    Signed-off-by: Arthur Schiwon <blizzz@arthur-schiwon.de>

--- build/fetch-material-icons.sh
+++ build/fetch-material-icons.sh
@@ -15,9 +15,11 @@ mkdir -p "$TARGET_PATH"
 
 cp $CLONE_PATH/LICENSE $TARGET_PATH/LICENSE
 
-cat $CLONE_PATH/meta.json | 
+cat $CLONE_PATH/meta.json |
     jq '.[]|select(.author == "Google" and (.name | contains("-") | not))' > $TARGET_PATH/meta.json
 
 cat $TARGET_PATH/meta.json |
     jq " .name | \"$CLONE_PATH/svg/\" + . + \".svg\"" -r  |
     xargs -I{} cp {} $PWD/img/material/
+
+find "${TARGET_PATH}" -name '*.svg' -exec sed -i 's_/></svg>_fill=\"#fff\" /></svg>_' {} + ;
