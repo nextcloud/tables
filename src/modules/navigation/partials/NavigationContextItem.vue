515191ff5ff27c04458f14ea1e981c3779d2703a
Cleopatra Enjeck M <patrathewhiz@gmail.com>
2024-03-07 07:51:54 +0100

    fix: table/view events, lint, add todos
    Signed-off-by: Cleopatra Enjeck M <patrathewhiz@gmail.com>

--- src/modules/navigation/partials/NavigationContextItem.vue
+++ src/modules/navigation/partials/NavigationContextItem.vue
@@ -6,14 +6,15 @@
 		:force-menu="true"
 		:to="'/context/' + parseInt(context.id)">
 		<template #icon>
-			<template v-if="context.emoji">
-				{{ context.emoji }}
+			<template v-if="context.iconName">
+				{{ context.iconName }}
 			</template>
 			<template v-else>
 				<Table :size="20" />
 			</template>
 		</template>
 		<template #actions>
+			<!-- TODO check if current user has right permissions before showing options -->
 			<NcActionButton
 				:close-after-click="true"
 				@click="editContext">
@@ -22,28 +23,16 @@
 				</template>
 				{{ t('tables', 'Edit context') }}
 			</NcActionButton>
-			<NcActionButton
-				icon="icon-delete"
-				:close-after-click="true"
-				@click="emit('tables:context:delete', context)">
-				{{ t('tables', 'Delete context') }}
-			</NcActionButton>
 		</template>
 	</NcAppNavigationItem>
 </template>
 <script>
-import { NcAppNavigationItem, NcActionButton, NcCounterBubble, NcAvatar } from '@nextcloud/vue'
+import { NcAppNavigationItem, NcActionButton } from '@nextcloud/vue'
 import '@nextcloud/dialogs/dist/index.css'
-import { getCurrentUser } from '@nextcloud/auth'
 import { mapGetters } from 'vuex'
-import { showError } from '@nextcloud/dialogs'
 import Table from 'vue-material-design-icons/Table.vue'
-import permissionsMixin from '../../../shared/components/ncTable/mixins/permissionsMixin.js'
-import Connection from 'vue-material-design-icons/Connection.vue'
-import PlaylistPlay from 'vue-material-design-icons/PlaylistPlay.vue'
 import { emit } from '@nextcloud/event-bus'
 import PlaylistEdit from 'vue-material-design-icons/PlaylistEdit.vue'
-import Import from 'vue-material-design-icons/Import.vue'
 
 export default {
 	name: 'NavigationContextItem',
@@ -53,25 +42,8 @@ export default {
 		// eslint-disable-next-line vue/no-reserved-component-names
 		Table,
 		NcAppNavigationItem,
-		NcCounterBubble,
 		NcActionButton,
-		Connection,
-		NcAvatar,
-		PlaylistPlay,
-		Import,
 	},
-
-	filters: {
-		truncate(string, num) {
-			if (string.length >= num) {
-				return string.substring(0, num) + '...'
-			} else {
-				return string
-			}
-		},
-	},
-
-	mixins: [permissionsMixin],
 
 	props: {
 		context: {
@@ -80,20 +52,8 @@ export default {
 		},
 	},
 
-	data() {
-		return {
-			showDeletionConfirmation: false,
-		}
-	},
-
 	computed: {
 		...mapGetters(['activeContext']),
-		getTranslatedDescription() {
-			return t('tables', 'Do you really want to delete the context "{context}"?', { context: this.context.name })
-		},
-		userId() {
-			return getCurrentUser().uid
-		},
 	},
 	methods: {
 		emit,
