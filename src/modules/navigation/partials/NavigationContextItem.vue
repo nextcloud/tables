761a3b5fe69e641b1d4ded6a0cddd0c47c9d4929
Cleopatra Enjeck M <patrathewhiz@gmail.com>
2024-03-12 13:23:56 +0100

    enh: modify styling and text for Context
    Signed-off-by: Cleopatra Enjeck M <patrathewhiz@gmail.com>

--- src/modules/navigation/partials/NavigationContextItem.vue
+++ src/modules/navigation/partials/NavigationContextItem.vue
@@ -7,7 +7,7 @@
 				{{ context.iconName }}
 			</template>
 			<template v-else>
-				<Table :size="20" />
+				<TableIcon :size="20" />
 			</template>
 		</template>
 		<template #actions>
@@ -24,7 +24,7 @@
 import { NcAppNavigationItem, NcActionButton } from '@nextcloud/vue'
 import '@nextcloud/dialogs/dist/index.css'
 import { mapGetters } from 'vuex'
-import Table from 'vue-material-design-icons/Table.vue'
+import TableIcon from 'vue-material-design-icons/Table.vue'
 import { emit } from '@nextcloud/event-bus'
 import PlaylistEdit from 'vue-material-design-icons/PlaylistEdit.vue'
 import permissionsMixin from '../../../shared/components/ncTable/mixins/permissionsMixin.js'
@@ -34,8 +34,7 @@ export default {
 
 	components: {
 		PlaylistEdit,
-		// eslint-disable-next-line vue/no-reserved-component-names
-		Table,
+		TableIcon,
 		NcAppNavigationItem,
 		NcActionButton,
 	},
