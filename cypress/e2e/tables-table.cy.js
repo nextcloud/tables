0febcbe8ccd1fdf7531146afb54725ea93b44e04
grnd-alt <salimbelakkaf@outlook.de>
2024-04-17 15:05:58 +0200

    adapt cypress test for tables description
    Signed-off-by: grnd-alt <salimbelakkaf@outlook.de>

--- cypress/e2e/tables-table.cy.js
+++ cypress/e2e/tables-table.cy.js
@@ -23,24 +23,28 @@ describe('Manage a table', () => {
 		cy.get('.tile').contains('ToDo').click({ force: true })
 		cy.get('.modal__content').should('be.visible')
 		cy.get('.modal__content input[type="text"]').clear().type('to do list')
+		cy.get('.modal__content #description-editor .tiptap.ProseMirror').type('to Do List description')
 		cy.contains('button', 'Create table').click()
 
 		cy.contains('button', 'Create row').should('be.visible')
 		cy.contains('h1', 'to do list').should('be.visible')
 		cy.contains('table th', 'Task').should('exist')
+		cy.contains('.paragraph-content', 'to Do List description').should('be.visible')
 	})
 
-	it('Update title', () => {
+	it('Update title And Description', () => {
 		cy.get('.app-navigation__list').contains('to do list').click({ force: true })
 		cy.get('[data-cy="customTableAction"] button').click()
 		cy.get('.action-button__text').contains('Edit table').click()
 
 		cy.get('.modal__content').should('be.visible')
 		cy.get('.modal-container input').last().clear().type('ToDo list')
+		cy.get('.modal__content #description-editor .tiptap.ProseMirror').type('Updated ToDo List description')
 		cy.get('.modal-container button').contains('Save').click()
 
 		cy.wait(10).get('.toastify.toast-success').should('be.visible')
 		cy.get('.app-navigation__list').contains('ToDo list').should('exist')
+		cy.contains('.paragraph-content', 'Updated ToDo List description').should('be.visible')
 	})
 
 	it('Delete', () => {
