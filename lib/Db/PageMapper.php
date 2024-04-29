526788a6794dfbabf51bbbe81b55e76d27631d08
Arthur Schiwon <blizzz@arthur-schiwon.de>
2024-04-29 17:46:28 +0200

    fix(Contexts): delete all related DB entries upon delete
    - related nodes
    - pages and their contents
    - navigation settings
    
    Signed-off-by: Arthur Schiwon <blizzz@arthur-schiwon.de>

--- lib/Db/PageMapper.php
+++ lib/Db/PageMapper.php
@@ -3,6 +3,7 @@
 namespace OCA\Tables\Db;
 
 use OCP\AppFramework\Db\QBMapper;
+use OCP\DB\QueryBuilder\IQueryBuilder;
 use OCP\IDBConnection;
 
 /** @template-extends QBMapper<Page> */
@@ -11,5 +12,30 @@ class PageMapper extends QBMapper {
 
 	public function __construct(IDBConnection $db) {
 		parent::__construct($db, $this->table, Page::class);
+	}
+
+	public function getPageIdsForContext(int $contextId): array {
+		$qb = $this->db->getQueryBuilder();
+
+		$qb->select('id')
+			->from($this->table)
+			->where($qb->expr()->eq('context_id', $qb->createNamedParameter($contextId, IQueryBuilder::PARAM_INT)));
+
+		$result = $qb->executeQuery();
+		$pageIds = [];
+		while ($row = $result->fetch()
+		) {
+			$pageIds[] = $row['id'];
+		}
+		return $pageIds;
+	}
+
+	public function deleteByPageId(int $pageId): int {
+		$qb = $this->db->getQueryBuilder();
+
+		$qb->delete($this->table)
+			->where($qb->expr()->eq('id', $qb->createNamedParameter($pageId, IQueryBuilder::PARAM_INT)));
+
+		return $qb->executeStatement();
 	}
 }
