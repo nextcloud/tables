a6bdf610ab163598c16ab6b68cac69069c3ab38d
Kiril Isakov <kirisakow@users.noreply.github.com>
2024-05-20 14:18:42 +0200

    add column type partials

---
+++ src/shared/components/ncTable/partials/columnTypePartials/forms/TextIPv6Form.vue
@@ -0,0 +1,84 @@
+<template>
+	<div style="width: 100%">
+		<!-- default -->
+		<div class="row">
+			<div class="fix-col-4">
+				{{ t('tables', 'Default') }}
+			</div>
+			<div class="fix-col-4 space-B">
+				<input v-model="mutableColumn.textDefault">
+			</div>
+		</div>
+
+		<!-- allowed pattern -->
+		<!--
+		<div class="row">
+			<div class="fix-col-1">
+				{{ t('tables', 'Allowed pattern (regex)') }}
+			</div>
+			<div class="fix-col-3 margin-bottom">
+				<input v-model="mutableColumn.textAllowedPattern">
+			</div>
+		</div>
+-->
+
+		<!-- max text length -->
+		<div class="row">
+			<div class="fix-col-4">
+				{{ t('tables', 'Maximum text length') }}
+			</div>
+			<div class="fix-col-4">
+				<input v-model="maxLength"
+					type="number"
+					step="1"
+					min="0">
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+
+import { translate as t } from '@nextcloud/l10n'
+
+export default {
+
+	props: {
+		column: {
+			type: Object,
+			default: null,
+		},
+		canSave: {
+			type: Boolean,
+			default: true,
+		},
+	},
+
+	data() {
+		return {
+			mutableColumn: this.column,
+		}
+	},
+
+	computed: {
+		maxLength: {
+			get() {
+				return this.mutableColumn.textMaxLength > 0 ? this.mutableColumn.textMaxLength : null
+			},
+			set(v) {
+				this.mutableColumn.textMaxLength = parseInt(v)
+			},
+		},
+	},
+
+	watch: {
+		column() {
+			this.mutableColumn = this.column
+		},
+	},
+
+	methods: {
+		t,
+	},
+}
+</script>
