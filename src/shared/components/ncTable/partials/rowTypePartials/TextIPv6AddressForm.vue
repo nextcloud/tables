11db2955b1a0ae94dcd037a1ef81c7e469a38f07
Kiril Isakov <kirisakow@users.noreply.github.com>
2024-05-21 09:42:08 +0200

    add row type partials

---
+++ src/shared/components/ncTable/partials/rowTypePartials/TextIPv6AddressForm.vue
@@ -0,0 +1,58 @@
+<template>
+	<RowFormWrapper :title="column.title" :mandatory="column.mandatory" :length="length" :max-length="column.textMaxLength" :description="column.description">
+		<input v-model="localValue" :maxlength="column.textMaxLength">
+	</RowFormWrapper>
+</template>
+
+<script>
+import RowFormWrapper from './RowFormWrapper.vue'
+
+export default {
+	components: {
+		RowFormWrapper,
+	},
+	props: {
+		column: {
+			type: Object,
+			default: null,
+		},
+		value: {
+			type: String,
+			default: null,
+		},
+	},
+	data() {
+		return {
+		}
+	},
+	computed: {
+		localValue: {
+			get() {
+				if (this.value === null) {
+					const newValue = this.column?.textDefault ? this.column.textDefault : ''
+					this.$emit('update:value', newValue)
+					return newValue
+				}
+				return this.value
+			},
+			set(v) { this.$emit('update:value', v) },
+		},
+		length() {
+			return (this.localValue) ? this.localValue.length : 0
+		},
+	},
+}
+</script>
+<style scoped>
+
+.hint-padding-left {
+	padding-left: 20px;
+}
+
+@media only screen and (max-width: 641px) {
+	.hint-padding-left {
+		padding-left: 0;
+	}
+}
+
+</style>
