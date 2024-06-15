d38606c26b98d37428949e8c1ac84d595a0a46c8
Julius Härtl <jus@bitgrid.net>
2024-06-15 11:51:54 +0200

    fix: Properly import material icons with vite
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- src/shared/components/ncIconPicker/mixins/svgHelper.js
+++ src/shared/components/ncIconPicker/mixins/svgHelper.js
@@ -4,20 +4,12 @@
  */
 export default {
 	methods: {
-		uriToSvg(dataUri) {
-			const pattern = /data:image\/svg\+xml;base64,/
-			const strippedUri = dataUri.replace(pattern, '')
-
-			return atob(strippedUri)
-		},
 		async getContextIcon(iconName) {
 			const { default: icon } = await import(
-				/* webpackChunkName: 'material-icons' */
-				/* webpackMode: 'lazy-once' */
-				`MaterialIcons/${iconName}.svg`
+				`./../../../../../img/material/${iconName}.svg?raw`
 			)
 
-			return this.uriToSvg(icon).replaceAll(/#fff/g, 'currentColor')
+			return icon.replaceAll(/#fff/g, 'currentColor')
 		},
 	},
 }
