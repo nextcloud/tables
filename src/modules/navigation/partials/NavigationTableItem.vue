030b57d996bde5ad89f81df8a22ad3d61a21952e
Julius Härtl <jus@bitgrid.net>
2024-06-27 08:06:18 +0200

    fix: Fix missing actions for empty default slot
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- src/modules/navigation/partials/NavigationTableItem.vue
+++ src/modules/navigation/partials/NavigationTableItem.vue
@@ -136,10 +136,12 @@
 				{{ t('tables', 'Delete table') }}
 			</NcActionButton>
 		</template>
-		<NavigationViewItem v-for="view in getViews"
-			:key="'view'+view.id"
-			:view="view"
-			:show-share-sender="false" />
+		<div>
+			<NavigationViewItem v-for="view in getViews"
+				:key="'view'+view.id"
+				:view="view"
+				:show-share-sender="false" />
+		</div>
 	</NcAppNavigationItem>
 </template>
 
