546c9f44c18d499046a9c2284e81dd98b756b04f
Julius Härtl <jus@bitgrid.net>
2024-08-02 13:27:53 +0200

    fix: Properly show displayname and status depending on type
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- src/shared/components/ncTable/partials/TableCellUsergroup.vue
+++ src/shared/components/ncTable/partials/TableCellUsergroup.vue
@@ -2,7 +2,7 @@
 	<!-- eslint-disable-next-line vue/no-v-html -->
 	<div v-if="value">
 		<div v-for="item in value" :key="item.id" class="inline usergroup-entry">
-			<NcUserBubble :user="item.id" :is-no-user="item.type !== 0" :display-name="item.displayName ?? item.id" :show-user-status="column.showUserStatus" :size="column.showUserStatus ? 34 : 20" :primary="isCurrentUser(item)" />
+			<NcUserBubble :user="item.id" :avatar-image="item.type === 1 ? 'icon-group' : undefined" :is-no-user="item.type !== 0" :display-name="item.displayName ?? item.id" :show-user-status="isUser(item) && column.showUserStatus" :size="column.showUserStatus ? 34 : 20" :primary="isCurrentUser(item)" />
 		</div>
 	</div>
 </template>
@@ -34,7 +34,10 @@ export default {
 	},
 	computed: {
 		isCurrentUser() {
-			return (user) => user.type === 0 && user.id === currentUser?.uid
+			return (item) => this.isUser(item) && item.id === currentUser?.uid
+		},
+		isUser() {
+			return (item) => item.type === 0
 		},
 	},
 }
