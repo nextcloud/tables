999bcceddf5e14c90c3bd9eb5a3985068105c026
Andy Scherzinger <info@andy-scherzinger.de>
2024-08-05 19:59:19 +0200

    chore: Add SPDX header
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,7 @@
+/**
+ * SPDX-FileCopyrightText: 2021 Florian Steffens
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
 const babelConfig = require('@nextcloud/babel-config')
 
 module.exports = babelConfig
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,7 @@
+/**
+ * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
 const path = require('path')
 const webpackConfig = require('@nextcloud/webpack-vue-config')
 
