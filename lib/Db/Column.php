6378c0436ac16c92dedbdfcd6826ed09b3a25851
Cleopatra Enjeck M <patrathewhiz@gmail.com>
2024-08-29 05:33:07 +0100

    feat: add support for teams in usergroup column
    Signed-off-by: Cleopatra Enjeck M <patrathewhiz@gmail.com>

--- lib/Db/Column.php
+++ lib/Db/Column.php
@@ -74,6 +74,8 @@ use OCP\AppFramework\Db\Entity;
  * @method setUsergroupSelectUsers(?bool $usergroupSelectUsers)
  * @method getUsergroupSelectGroups(): bool
  * @method setUsergroupSelectGroups(?bool $usergroupSelectGroups)
+ * @method getUsergroupSelectTeams(): bool
+ * @method setUsergroupSelectTeams(?bool $usergroupSelectTeams)
  * @method getShowUserStatus(): bool
  * @method setShowUserStatus(?bool $showUserStatus)
  */
@@ -130,6 +132,7 @@ class Column extends Entity implements JsonSerializable {
 	protected ?bool $usergroupMultipleItems = null;
 	protected ?bool $usergroupSelectUsers = null;
 	protected ?bool $usergroupSelectGroups = null;
+	protected ?bool $usergroupSelectTeams = null;
 	protected ?bool $showUserStatus = null;
 
 	public function __construct() {
@@ -150,6 +153,7 @@ class Column extends Entity implements JsonSerializable {
 		$this->addType('usergroupMultipleItems', 'boolean');
 		$this->addType('usergroupSelectUsers', 'boolean');
 		$this->addType('usergroupSelectGroups', 'boolean');
+		$this->addType('usergroupSelectTeams', 'boolean');
 		$this->addType('showUserStatus', 'boolean');
 	}
 
@@ -252,6 +256,7 @@ class Column extends Entity implements JsonSerializable {
 			'usergroupMultipleItems' => $this->usergroupMultipleItems,
 			'usergroupSelectUsers' => $this->usergroupSelectUsers,
 			'usergroupSelectGroups' => $this->usergroupSelectGroups,
+			'usergroupSelectTeams' => $this->usergroupSelectTeams,
 			'showUserStatus' => $this->showUserStatus,
 		];
 	}
