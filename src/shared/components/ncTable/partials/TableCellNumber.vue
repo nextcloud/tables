9f064a1c832872fa010b1ce7ca8db9bc9729b500
Cleopatra Enjeck M. <patrathewhiz@gmail.com>
2025-04-03 06:29:58 +0100

    feat: Add inline editing to some cell types
    Signed-off-by: Cleopatra Enjeck M. <patrathewhiz@gmail.com>

--- src/shared/components/ncTable/partials/TableCellNumber.vue
+++ src/shared/components/ncTable/partials/TableCellNumber.vue
@@ -3,44 +3,166 @@
   - SPDX-License-Identifier: AGPL-3.0-or-later
 -->
 <template>
-	<div>
-		{{ column.numberPrefix }}{{ getValue }}{{ column.numberSuffix }}
+	<div class="cell-number">
+		<div v-if="!isEditing" @dblclick="startEditing">
+			{{ column.numberPrefix }}{{ getValue }}{{ column.numberSuffix }}
+		</div>
+		<div v-else class="editing-container">
+			<input
+				ref="input"
+				v-model="editValue"
+				type="number"
+				:min="getMin"
+				:max="getMax"
+				:step="getStep"
+				:disabled="localLoading"
+				class="cell-input"
+				@blur="saveChanges"
+				@keyup.enter="saveChanges"
+				@keyup.esc="cancelEdit">
+			<div v-if="localLoading" class="icon-loading" />
+		</div>
 	</div>
 </template>
 
 <script>
+import { showError } from '@nextcloud/dialogs'
+import { translate as t } from '@nextcloud/l10n'
+import { mapActions, mapState } from 'pinia'
+import { useDataStore } from '../../../../store/data.js'
 
 export default {
 	name: 'TableCellNumber',
+
 	props: {
 		column: {
 			type: Object,
-			default: () => {},
+			required: true,
 		},
 		rowId: {
 			type: Number,
-			default: null,
+			required: true,
 		},
 		value: {
 			type: Number,
 			default: null,
 		},
 	},
+
+	data() {
+		return {
+			isEditing: false,
+			editValue: '',
+			localLoading: false,
+		}
+	},
+
 	computed: {
+		...mapState(useDataStore, {
+			rowMetadata(state) {
+				return state.getRowMetadata(this.rowId)
+			},
+		}),
+
 		getValue() {
 			if (this.value === null) {
 				return null
 			}
 			return this.value.toFixed(this.column?.numberDecimals)
 		},
+
+		getStep() {
+			return Math.pow(10, -(this.column?.numberDecimals || 0))
+		},
+		getMin() {
+			if (this.column?.numberMin !== undefined) {
+				return this.column.numberMin
+			} else {
+				return null
+			}
+		},
+		getMax() {
+			if (this.column?.numberMax !== undefined) {
+				return this.column.numberMax
+			} else {
+				return null
+			}
+		},
+	},
+
+	methods: {
+		...mapActions(useDataStore, ['updateRow']),
+
+		startEditing() {
+			this.editValue = this.value
+			this.isEditing = true
+			this.$nextTick(() => {
+				this.$refs.input.focus()
+			})
+		},
+
+		async saveChanges() {
+			if (Number(this.editValue) === this.value) {
+				this.isEditing = false
+				return
+			}
+
+			this.localLoading = true
+
+			const data = [{
+				columnId: this.column.id,
+				value: this.editValue === '' ? null : Number(this.editValue),
+			}]
+
+			const res = await this.updateRow({
+				id: this.rowId,
+				isView: this.rowMetadata.isView,
+				elementId: this.rowMetadata.elementId,
+				data,
+			})
+
+			if (!res) {
+				showError(t('tables', 'Could not update cell'))
+				this.cancelEdit()
+			} else {
+				this.$emit('update:value', Number(this.editValue))
+			}
+
+			this.localLoading = false
+			this.isEditing = false
+		},
+
+		cancelEdit() {
+			this.isEditing = false
+			this.editValue = this.value
+		},
 	},
 }
 </script>
 
-<style lang="scss" scoped>
-
-div {
-	text-align: right;
+<style scoped>
+.cell-number {
+    width: 100%;
+    text-align: right;
 }
 
+.editing-container {
+    position: relative;
+    width: 100%;
+}
+
+.cell-input {
+    width: 100%;
+    border: 1px solid var(--color-border);
+    border-radius: var(--border-radius);
+    padding: 4px 8px;
+    text-align: right;
+}
+
+.icon-loading {
+    position: absolute;
+    right: 8px;
+    top: 50%;
+    transform: translateY(-50%);
+}
 </style>
