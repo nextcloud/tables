c7815b7bb5cf9921b222eae539bc582231f93397
Cleopatra Enjeck M. <patrathewhiz@gmail.com>
2025-04-08 05:27:36 +0100

    enh: Use large size edit/create modal
    Signed-off-by: Cleopatra Enjeck M. <patrathewhiz@gmail.com>

--- src/modules/modals/CreateRow.vue
+++ src/modules/modals/CreateRow.vue
@@ -5,6 +5,7 @@
 <template>
 	<NcDialog v-if="showModal"
 		:name="t('tables', 'Create row')"
+		size="large"
 		data-cy="createRowModal"
 		@closing="actionCancel">
 		<div class="modal__content" @keydown="onKeydown">
