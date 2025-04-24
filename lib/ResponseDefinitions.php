b90b1a5bd12d1d6eaf7661c6ba6421cff63f7dbc
Kostiantyn Miakshyn <molodchick@gmail.com>
2025-04-24 19:44:34 +0200

    Enhancement: Add validation for unique values for short text fields
    Signed-off-by: Kostiantyn Miakshyn <molodchick@gmail.com>

--- lib/ResponseDefinitions.php
+++ lib/ResponseDefinitions.php
@@ -122,6 +122,7 @@ namespace OCA\Tables;
  *  textDefault: string,
  *  textAllowedPattern: string,
  *  textMaxLength: int,
+ *  textUnique: bool,
  *  selectionOptions: string,
  *  selectionDefault: string,
  *  datetimeDefault: string,
