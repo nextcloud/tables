e7035e471fc613006adc7901c9b764ce989dc406
Enjeck C. <patrathewhiz@gmail.com>
2025-06-07 13:16:29 +0100

    fix: Properly display Tables smartpicker content
    Signed-off-by: Enjeck C. <patrathewhiz@gmail.com>

--- src/reference.js
+++ src/reference.js
@@ -26,12 +26,19 @@ registerWidget('tables_content', async (el, { richObjectType, richObject, access
 	const { default: TableReferenceWidget } = interactive
 		? await import(/* webpackChunkName: "reference-table-lazy" */'./views/ContentReferenceWidget.vue')
 		: await import(/* webpackChunkName: "reference-table-lazy" */'./views/LinkReferenceWidget.vue')
-	const { default: store } = await import(/* webpackChunkName: 'store' */ './store/store.js')
-	const { default: data } = await import(/* webpackChunkName: 'store' */ './store/data.js')
+	
+	// Import Pinia and stores
+	const { createPinia, setActivePinia } = await import('pinia')
+	const { useTablesStore } = await import(/* webpackChunkName: 'store' */ './store/store.js')
+	const { useDataStore } = await import(/* webpackChunkName: 'store' */ './store/data.js')
 
-	store.data = data
+	// Create and set up Pinia instance
+	const pinia = createPinia()
+	setActivePinia(pinia)
 
 	Vue.mixin({ methods: { t, n } })
+	Vue.use(pinia)
+	
 	const Widget = Vue.extend(TableReferenceWidget)
 	new Widget({
 		propsData: {
@@ -39,13 +46,20 @@ registerWidget('tables_content', async (el, { richObjectType, richObject, access
 			richObject,
 			accessible,
 		},
-		store,
+		pinia,
 	}).$mount(el)
 }, () => {}, { hasInteractiveView: true })
 
 registerCustomPickerElement('tables-ref-tables', async (el, { providerId, accessible }) => {
 	const { default: Vue } = await import('vue')
 	const { default: TablesSmartPicker } = await import('./views/SmartPicker.vue')
+	
+	// Import Pinia
+	const { createPinia } = await import('pinia')
+	const pinia = createPinia()
+	
+	Vue.use(pinia)
+	
 	const Element = Vue.extend(TablesSmartPicker)
 	const vueElement = new Element({
 		propsData: {
