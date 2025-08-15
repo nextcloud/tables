85c62c1f94a91b19e3c08962acc228ca9ed9f52e
Luka Trovic <luka@nextcloud.com>
2025-08-15 10:32:07 +0200

    feat: activity support
    Signed-off-by: Luka Trovic <luka@nextcloud.com>

---
+++ src/modules/sidebar/sections/SidebarActivity.vue
@@ -0,0 +1,27 @@
+<!--
+	- SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+	- SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+<template>
+	<ActivityList v-if="$parent.isActive"
+		filter="tables"
+		:object-id="activeElement.id"
+		object-type="tables_table"
+		type="tables" />
+</template>
+
+<script>
+import ActivityList from '../../../shared/components/ActivityList.vue'
+import { mapState } from 'pinia'
+import { useTablesStore } from '../../../store/store.js'
+
+export default {
+	name: 'SidebarActivity',
+	components: {
+		ActivityList,
+	},
+	computed: {
+		...mapState(useTablesStore, ['activeElement']),
+	},
+}
+</script>
