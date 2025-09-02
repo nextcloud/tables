c09a5eb18f2d2ed4e66f8684d43264f81c488f81
silver <s.szmajduch@posteo.de>
2025-09-02 15:10:46 +0200

    cypress tests for is-not-equal and does-not-contain
    Signed-off-by: silver <s.szmajduch@posteo.de>

--- cypress/e2e/view-filtering-selection.cy.js
+++ cypress/e2e/view-filtering-selection.cy.js
@@ -186,6 +186,43 @@ describe('Filtering in a view by selection columns', () => {
 		})
 	})
 
+	it('Filter view for single selection - is not equal', () => {
+		cy.loadTable('View filtering test table')
+
+		const title = 'Filter for single selection - is not equal'
+		cy.get('[data-cy="customTableAction"] button').click()
+		cy.get('.v-popper__popper li button span').contains('Create view').click({ force: true })
+		cy.get('.modal-container #settings-section_title input').type(title)
+
+		// add filter
+		cy.get('[data-cy="filterFormFilterGroupBtn"]').click()
+		cy.get('.modal-container .filter-group .v-select.select').eq(0).click()
+		cy.get('ul.vs__dropdown-menu li span[title="selection"]').click()
+		cy.get('.modal-container .filter-group .v-select.select').eq(1).click()
+		cy.get('ul.vs__dropdown-menu li span[title="Is not equal"]').click()
+		cy.get('.modal-container .filter-group .v-select.select').eq(2).click()
+		cy.get('ul.vs__dropdown-menu li span[title="sel2"]').click()
+
+		// save view
+		cy.intercept({ method: 'POST', url: '**/apps/tables/view' }).as('createView')
+		cy.intercept({ method: 'PUT', url: '**/apps/tables/view/*' }).as('updateView')
+		cy.contains('button', 'Create View').click()
+		cy.wait('@createView')
+		cy.wait('@updateView')
+		cy.contains('.app-navigation-entry-link span', title).should('exist')
+
+		// check for expected rows
+		const expected = ['first row', 'third row', 'fifths row', 'sixths row']
+		expected.forEach(item => {
+			cy.get('.custom-table table tr td div').contains(item).should('be.visible')
+		})
+
+		const unexpected = ['second row', 'fourth row', 'sevenths row']
+		unexpected.forEach(item => {
+			cy.get('.custom-table table tr td div').contains(item).should('not.exist')
+		})
+	})
+
 	it('Filter view for multi selection - contains', () => {
 		cy.loadTable('View filtering test table')
 
@@ -271,6 +308,86 @@ describe('Filtering in a view by selection columns', () => {
 
 		// ## not expected
 		const unexpected = ['second row', 'third row', 'fourth row', 'fifths row', 'sixths row']
+		unexpected.forEach(item => {
+			cy.get('.custom-table table tr td div').contains(item).should('not.exist')
+		})
+	})
+
+	it('Filter view for single selection - does not contain', () => {
+		cy.loadTable('View filtering test table')
+
+		// # create view with filter
+		const title = 'Filter does not contain sel2'
+		cy.get('[data-cy="customTableAction"] button').click()
+		cy.get('.v-popper__popper li button span').contains('Create view').click({ force: true })
+		cy.get('.modal-container #settings-section_title input').type(title)
+
+		// ## add filter
+		cy.get('[data-cy="filterFormFilterGroupBtn"]').click()
+		cy.get('.modal-container .filter-group .v-select.select').eq(0).click()
+		cy.get('ul.vs__dropdown-menu li span[title="selection"]').click()
+		cy.get('.modal-container .filter-group .v-select.select').eq(1).click()
+		cy.get('ul.vs__dropdown-menu li span[title="Does not contain"]').click()
+		cy.get('.modal-container .filter-group .v-select.select').eq(2).click()
+		cy.get('ul.vs__dropdown-menu li span[title="sel2"]').click()
+
+		// ## save view
+		cy.intercept({ method: 'POST', url: '**/apps/tables/view' }).as('createView')
+		cy.intercept({ method: 'PUT', url: '**/apps/tables/view/*' }).as('updateView')
+		cy.contains('button', 'Create View').click()
+		cy.wait('@createView')
+		cy.wait('@updateView')
+		cy.contains('.app-navigation-entry-link span', title).should('exist')
+
+		// # check for expected rows
+		// rows that **do not contain sel2**
+		const expected = ['first row', 'third row', 'fourth row', 'fifths row', 'sixths row']
+		expected.forEach(item => {
+			cy.get('.custom-table table tr td div').contains(item).should('be.visible')
+		})
+
+		// rows that **contain sel2** should not be visible
+		const unexpected = ['second row', 'sevenths row']
+		unexpected.forEach(item => {
+			cy.get('.custom-table table tr td div').contains(item).should('not.exist')
+		})
+	})
+
+	it('Filter view for multi selection - does not contain', () => {
+		cy.loadTable('View filtering test table')
+
+		// # create view with filter
+		const title = 'Filter multi selection does not contain A'
+		cy.get('[data-cy="customTableAction"] button').click()
+		cy.get('.v-popper__popper li button span').contains('Create view').click({ force: true })
+		cy.get('.modal-container #settings-section_title input').type(title)
+
+		// ## add filter
+		cy.get('[data-cy="filterFormFilterGroupBtn"]').click()
+		cy.get('.modal-container .filter-group .v-select.select').eq(0).click()
+		cy.get('ul.vs__dropdown-menu li span[title="multi selection"]').click()
+		cy.get('.modal-container .filter-group .v-select.select').eq(1).click()
+		cy.get('ul.vs__dropdown-menu li span[title="Does not contain"]').click()
+		cy.get('.modal-container .filter-group .v-select.select').eq(2).click()
+		cy.get('ul.vs__dropdown-menu li span[title="A"]').click()
+
+		// ## save view
+		cy.intercept({ method: 'POST', url: '**/apps/tables/view' }).as('createView')
+		cy.intercept({ method: 'PUT', url: '**/apps/tables/view/*' }).as('updateView')
+		cy.contains('button', 'Create View').click()
+		cy.wait('@createView')
+		cy.wait('@updateView')
+		cy.contains('.app-navigation-entry-link span', title).should('exist')
+
+		// # check for expected rows
+		// rows that **do not contain A** in multi selection
+		const expected = ['third row', 'fifths row', 'sixths row']
+		expected.forEach(item => {
+			cy.get('.custom-table table tr td div').contains(item).should('be.visible')
+		})
+
+		// rows that **contain A** should not be visible
+		const unexpected = ['first row', 'fourth row', 'sevenths row']
 		unexpected.forEach(item => {
 			cy.get('.custom-table table tr td div').contains(item).should('not.exist')
 		})
