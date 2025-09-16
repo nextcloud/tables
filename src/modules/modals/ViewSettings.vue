f78a5c9560629611f7d43d4cab02ffa3d0b331b2
silver <s.szmajduch@posteo.de>
2025-09-16 15:58:18 +0200

    correct typo in viewsettings
    Signed-off-by: silver <s.szmajduch@posteo.de>

--- src/modules/modals/ViewSettings.vue
+++ src/modules/modals/ViewSettings.vue
@@ -154,7 +154,7 @@ export default {
 		saveText() {
 			if (this.createView) {
 				return t('tables', 'Create View')
-			} else if (this.viewSettings) {
+			} else if (this.viewSetting) {
 				return t('tables', 'Save modified View')
 			} else {
 				return t('tables', 'Save View')
