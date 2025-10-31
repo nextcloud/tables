26c255cd62da92c2f5854e5f052e25dff11accc5
Kostiantyn Miakshyn <molodchick@gmail.com>
2025-10-31 15:00:49 +0100

    Enhancement: Make tables import asynchronous
    Signed-off-by: Kostiantyn Miakshyn <molodchick@gmail.com>

--- lib/Service/PermissionsService.php
+++ lib/Service/PermissionsService.php
@@ -628,7 +628,7 @@ class PermissionsService {
 		try {
 			$userId = $this->preCheckUserId($userId);
 		} catch (InternalError $e) {
-			$e = new \Exception('Cannot pre check the user id');
+			$e = new \Exception('Cannot pre check the user id', 0, $e);
 			$this->logger->error($e->getMessage(), ['exception' => $e]);
 			return false;
 		}
