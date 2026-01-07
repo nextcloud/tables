391084a1ecb7f36cc3c1cc2510fcb72d6a12e529
Enjeck C. <patrathewhiz@gmail.com>
2026-01-07 03:40:53 +0000

    enh: Use emoji, title, description
    Signed-off-by: Enjeck C. <patrathewhiz@gmail.com>

--- src/modules/main/sections/PublicElement.vue
+++ src/modules/main/sections/PublicElement.vue
@@ -3,37 +3,32 @@
   - SPDX-License-Identifier: AGPL-3.0-or-later
 -->
 <template>
-	<div class="table-wrapper">
-		<EmptyView v-if="columns.length === 0" :view="element" />
-		<TableView v-else
-			:rows="rows"
-			:columns="columns"
-			:element="element"
-			:can-read-rows="true"
-			:can-create-rows="false"
-			:can-edit-rows="false"
-			:can-delete-rows="false"
-			:can-create-columns="false"
-			:can-edit-columns="false"
-			:can-delete-columns="false"
-			:can-delete-table="false">
-			<template #actions>
-				<NcActions :force-menu="true" type="tertiary">
-					<NcActionButton :close-after-click="true"
-						icon="icon-download"
-						data-cy="dataTableExportBtn"
-						@click="$emit('download-csv')">
-						{{ t('tables', 'Export as CSV') }}
-					</NcActionButton>
-				</NcActions>
-			</template>
-		</TableView>
+	<div>
+		<ElementTitle :active-element="element" :is-table="false" :view-setting.sync="localViewSetting" />
+		<TableDescription :description="element.description" :read-only="true" />
+		<div class="table-wrapper">
+			<EmptyView v-if="columns.length === 0" :view="element" />
+			<TableView v-else :rows="rows" :columns="columns" :element="element" :can-read-rows="true"
+				:can-create-rows="false" :can-edit-rows="false" :can-delete-rows="false" :can-create-columns="false"
+				:can-edit-columns="false" :can-delete-columns="false" :can-delete-table="false">
+				<template #actions>
+					<NcActions :force-menu="true" type="tertiary">
+						<NcActionButton :close-after-click="true" icon="icon-download" data-cy="dataTableExportBtn"
+							@click="$emit('download-csv')">
+							{{ t('tables', 'Export as CSV') }}
+						</NcActionButton>
+					</NcActions>
+				</template>
+			</TableView>
+		</div>
 	</div>
 </template>
 
 <script>
 import TableView from '../partials/TableView.vue'
 import EmptyView from './EmptyView.vue'
+import TableDescription from './TableDescription.vue'
+import ElementTitle from './ElementTitle.vue'
 import { NcActions, NcActionButton } from '@nextcloud/vue'
 
 export default {
@@ -43,6 +38,8 @@ export default {
 		TableView,
 		NcActions,
 		NcActionButton,
+		TableDescription,
+		ElementTitle,
 	},
 
 	props: {
