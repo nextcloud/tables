324063933d8328681d214a7d0add5a678e11a906
Kostiantyn Miakshyn <molodchick@gmail.com>
2026-01-09 13:45:05 +0100

    feat: add relation column type
    Signed-off-by: Kostiantyn Miakshyn <molodchick@gmail.com>

--- src/types/openapi/openapi.ts
+++ src/types/openapi/openapi.ts
@@ -2670,7 +2670,7 @@ export interface operations {
                      * @description Column main type
                      * @enum {string}
                      */
-                    readonly type: "text" | "number" | "datetime" | "select" | "usergroup";
+                    readonly type: "text" | "number" | "datetime" | "select" | "usergroup" | "relation";
                     /** @description Column sub type */
                     readonly subtype?: string | null;
                     /** @description Is the column mandatory */
@@ -2930,7 +2930,7 @@ export interface operations {
                      * @description Column main type
                      * @enum {string}
                      */
-                    readonly type: "text" | "number" | "datetime" | "select" | "usergroup";
+                    readonly type: "text" | "number" | "datetime" | "select" | "usergroup" | "relation";
                     /** @description Column sub type */
                     readonly subtype?: string | null;
                     /** @description Is the column mandatory */
