b463892c924bcb8b44889560a9ef5dd006051f3e
Kostiantyn Miakshyn <molodchick@gmail.com>
2026-01-09 13:45:05 +0100

    feat: add relation column type
    Signed-off-by: Kostiantyn Miakshyn <molodchick@gmail.com>

---
+++ lib/Service/ColumnTypes/RelationBusiness.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OCA\Tables\Service\ColumnTypes;
+
+use OCA\Tables\Db\Column;
+use OCA\Tables\Service\RelationService;
+use Psr\Log\LoggerInterface;
+
+class RelationBusiness extends SuperBusiness implements IColumnTypeBusiness {
+
+	public function __construct(
+		LoggerInterface $logger,
+		private RelationService $relationService,
+	) {
+		parent::__construct($logger);
+	}
+
+	/**
+	 * @param mixed $value (array|string|null)
+	 * @param Column|null $column
+	 * @return string
+	 */
+	public function parseValue($value, ?Column $column = null): string {
+		if (!$column) {
+			$this->logger->warning('No column given, but expected on ' . __FUNCTION__ . ' within ' . __CLASS__, ['exception' => new \Exception()]);
+			return '';
+		}
+
+		$relationData = $this->relationService->getRelationData($column);
+		// try to find value by label
+		$matchingRelation = array_filter($relationData, fn (array $relation) => $relation['label'] === $value);
+		if (!empty($matchingRelation)) {
+			return json_encode(reset($matchingRelation)['id']);
+		}
+
+		// if not found, try to find by id
+		if (is_numeric($value) && isset($relationData[(int)$value])) {
+			return json_encode($value);
+		}
+
+		return '';
+	}
+
+	/**
+	 * @param mixed $value (array|string|null)
+	 * @param Column|null $column
+	 * @return bool
+	 */
+	public function canBeParsed($value, ?Column $column = null): bool {
+		if (!$column) {
+			$this->logger->warning('No column given, but expected on ' . __FUNCTION__ . ' within ' . __CLASS__, ['exception' => new \Exception()]);
+			return false;
+		}
+		if ($value === null) {
+			return true;
+		}
+
+		$relationData = $this->relationService->getRelationData($column);
+		// try to find value by label
+		$matchingRelation = array_filter($relationData, fn (array $relation) => $relation['label'] === $value);
+		if (!empty($matchingRelation)) {
+			return true;
+		}
+		// if not found, try to find by id
+		if (is_numeric($value) && isset($relationData[(int)$value])) {
+			return true;
+		}
+
+		return false;
+	}
+}
