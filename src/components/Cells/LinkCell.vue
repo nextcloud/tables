b08290a277c93df380342906c7a9783acddd5976
aryanjsingh <aryanjsingh07@gmail.com>
2026-04-04 07:42:14 +0530

    fix: Allow selecting system users in contact link column

---
+++ src/components/Cells/LinkCell.vue
@@ -0,0 +1,25 @@
+			const types = allowedTypes.flatMap(type => {
+				switch (type) {
+				case 'File':
+					return ['file']
+				case 'Talk':
+					return ['talk']
+				case 'Contact':
+					return ['contact', 'user']
+				case 'User':
+					return ['user']
+				case 'Group':
+					return ['group']
+				case 'Circle':
+					return ['circle']
+				case 'Deck':
+					return ['deck']
+				case 'Calendar':
+					return ['calendar']
+				case 'Mail':
+					return ['mail']
+				default:
+					return []
+				}
+			})
+			return [...new Set(types)]
