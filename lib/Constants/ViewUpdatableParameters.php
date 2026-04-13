97f12ed2a4451679f642e103327eb2a856cd3c06
Rello <Rello@users.noreply.github.com>
2026-04-13 22:49:13 +0200

    Add files via upload
    Signed-off-by: Rello <Rello@users.noreply.github.com>

--- lib/Constants/ViewUpdatableParameters.php
+++ lib/Constants/ViewUpdatableParameters.php
@@ -17,4 +17,6 @@ enum ViewUpdatableParameters: string {
 	case FILTER = 'filter';
 	case COLUMN_SETTINGS = 'columns';
 	case LAYOUT = 'layout';
+	case CARD_BACKGROUND_SOURCE = 'cardBackgroundSource';
+	case CARD_TITLE_SOURCE = 'cardTitleSource';
 }
