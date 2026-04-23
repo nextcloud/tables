d4657ca7cdfb06f50d4016d6a14f9fd543bff6f7
Enjeck C. <patrathewhiz@gmail.com>
2026-04-23 18:16:27 +0100

    feat(test): Add unit test for reordering in context
    Signed-off-by: Enjeck C. <patrathewhiz@gmail.com>

---
+++ tests/unit/Service/ContextServiceTest.php
@@ -0,0 +1,164 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OCA\Tables\Service;
+
+use OCA\Tables\Db\Context;
+use OCA\Tables\Db\ContextMapper;
+use OCA\Tables\Db\ContextNodeRelationMapper;
+use OCA\Tables\Db\Page;
+use OCA\Tables\Db\PageContent;
+use OCA\Tables\Db\PageContentMapper;
+use OCA\Tables\Db\PageMapper;
+use OCP\EventDispatcher\IEventDispatcher;
+use OCP\IDBConnection;
+use OCP\INavigationManager;
+use OCP\IURLGenerator;
+use OCP\IUserManager;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
+
+final class ContextServiceTest extends TestCase {
+	private ContextService $service;
+	private MockObject $contextMapper;
+	private MockObject $contextNodeRelMapper;
+	private MockObject $pageMapper;
+	private MockObject $pageContentMapper;
+
+	protected function setUp(): void {
+		parent::setUp();
+
+		$this->contextMapper = $this->createMock(ContextMapper::class);
+		$this->contextNodeRelMapper = $this->createMock(ContextNodeRelationMapper::class);
+		$this->pageMapper = $this->createMock(PageMapper::class);
+		$this->pageContentMapper = $this->createMock(PageContentMapper::class);
+		$logger = $this->createMock(LoggerInterface::class);
+		$permissionsService = $this->createMock(PermissionsService::class);
+		$userManager = $this->createMock(IUserManager::class);
+		$eventDispatcher = $this->createMock(IEventDispatcher::class);
+		$dbConnection = $this->createMock(IDBConnection::class);
+		$shareService = $this->createMock(ShareService::class);
+		$navigationManager = $this->createMock(INavigationManager::class);
+		$urlGenerator = $this->createMock(IURLGenerator::class);
+
+		$this->service = new ContextService(
+			$this->contextMapper,
+			$this->contextNodeRelMapper,
+			$this->pageMapper,
+			$this->pageContentMapper,
+			$logger,
+			$permissionsService,
+			$userManager,
+			$eventDispatcher,
+			$dbConnection,
+			$shareService,
+			false,
+			$navigationManager,
+			$urlGenerator,
+		);
+	}
+
+	public function testUpdateReordersStartPageContentsFromSubmittedNodes(): void {
+		$context = new Context();
+		$context->id = 7;
+		$context->setNodes([
+			101 => [
+				'id' => 101,
+				'node_type' => 0,
+				'node_id' => 11,
+				'permissions' => 1,
+			],
+			102 => [
+				'id' => 102,
+				'node_type' => 0,
+				'node_id' => 12,
+				'permissions' => 1,
+			],
+		]);
+		$context->setPages([
+			301 => [
+				'id' => 301,
+				'page_type' => Page::TYPE_STARTPAGE,
+				'content' => [
+					501 => [
+						'id' => 501,
+						'node_rel_id' => 101,
+						'order' => 10,
+					],
+					502 => [
+						'id' => 502,
+						'node_rel_id' => 102,
+						'order' => 20,
+					],
+				],
+			],
+		]);
+
+		$pageContentsById = [
+			501 => $this->buildPageContent(501, 301, 101, 10),
+			502 => $this->buildPageContent(502, 301, 102, 20),
+		];
+		$updatedOrders = [];
+
+		$this->contextMapper
+			->expects($this->once())
+			->method('findById')
+			->with(7, 'user-1')
+			->willReturn($context);
+
+		$this->pageContentMapper
+			->expects($this->exactly(2))
+			->method('findById')
+			->willReturnCallback(static fn (int $contentId): PageContent => $pageContentsById[$contentId]);
+
+		$this->pageContentMapper
+			->expects($this->exactly(2))
+			->method('update')
+			->willReturnCallback(function (PageContent $pageContent) use (&$updatedOrders): PageContent {
+				$updatedOrders[$pageContent->getId()] = $pageContent->getOrder();
+				return $pageContent;
+			});
+
+		$this->contextMapper
+			->expects($this->once())
+			->method('update')
+			->with($context)
+			->willReturn($context);
+
+		$updatedContext = $this->service->update(7, 'user-1', null, null, null, [
+			[
+				'id' => 12,
+				'type' => 0,
+				'permissions' => 1,
+			],
+			[
+				'id' => 11,
+				'type' => 0,
+				'permissions' => 1,
+			],
+		]);
+
+		self::assertCount(2, $updatedOrders);
+		self::assertSame(20, $updatedOrders[501]);
+		self::assertSame(10, $updatedOrders[502]);
+		self::assertSame(20, $updatedContext->getPages()[301]['content'][501]['order']);
+		self::assertSame(10, $updatedContext->getPages()[301]['content'][502]['order']);
+	}
+
+	private function buildPageContent(int $id, int $pageId, int $nodeRelId, int $order): PageContent {
+		$pageContent = new PageContent();
+		$pageContent->id = $id;
+		$pageContent->setPageId($pageId);
+		$pageContent->setNodeRelId($nodeRelId);
+		$pageContent->setOrder($order);
+
+		return $pageContent;
+	}
+}
