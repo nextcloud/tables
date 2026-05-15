aa272d163e2291fbfaf429165b9e87c0dc1b286f
tomaioo <hongsaygio.com@gmail.com>
2026-05-14 23:22:08 -0700

    fix(security): 2 improvements across 2 files
    - Security: Missing Authorization Check in V1Api
    - Security: Potential Array Index Out of Bounds in SelectionColumnQB
    
    Signed-off-by: tomaioo <203048277+tomaioo@users.noreply.github.com>

--- lib/Db/ColumnTypes/SelectionColumnQB.php
+++ lib/Db/ColumnTypes/SelectionColumnQB.php
@@ -13,8 +13,12 @@ class SelectionColumnQB extends SuperColumnQB implements IColumnTypeQB {
 	public function passSearchValue(IQueryBuilder $qb, string $unformattedSearchValue, string $operator, string $searchValuePlaceHolder): void {
 		if (substr($unformattedSearchValue, 0, 1) === '@') {
 			$parts = explode('-', $unformattedSearchValue);
-			$selectedId = intval($parts[2]);
-			$qb->setParameter($searchValuePlaceHolder, $selectedId, IQueryBuilder::PARAM_INT);
+			if (count($parts) >= 3) {
+				$selectedId = intval($parts[2]);
+				$qb->setParameter($searchValuePlaceHolder, $selectedId, IQueryBuilder::PARAM_INT);
+			} else {
+				$qb->setParameter($searchValuePlaceHolder, $unformattedSearchValue, IQueryBuilder::PARAM_STR);
+			}
 		} else {
 			$qb->setParameter($searchValuePlaceHolder, $unformattedSearchValue, IQueryBuilder::PARAM_STR);
 		}
