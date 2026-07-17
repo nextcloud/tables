8b260b1ec5d48098c1d359cae7d3ba67670ced64
Arthur Schiwon <blizzz@arthur-schiwon.de>
2026-07-17 16:57:56 +0200

    fix(Export): also return uuid for selection options
    Signed-off-by: Arthur Schiwon <blizzz@arthur-schiwon.de>

--- lib/Model/SelectionOption.php
+++ lib/Model/SelectionOption.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
 namespace OCA\Tables\Model;
 
 class SelectionOption implements \JsonSerializable {
+	private string $uuid;
 
 	public function __construct(
 		private readonly int $key,
@@ -17,6 +18,9 @@ class SelectionOption implements \JsonSerializable {
 	) {
 	}
 
+	/**
+	 * @psalm-param array{id: int|string, label: string, uuid?: string} $data
+	 */
 	public static function createFromInputArray(array $data, bool $allowPassingUuid = false): self {
 		if (!isset($data['id']) || !is_numeric($data['id'])) {
 			throw new \InvalidArgumentException('Only integer keys are allowed for options');
@@ -30,7 +34,11 @@ class SelectionOption implements \JsonSerializable {
 			throw new \InvalidArgumentException('It is forbidden to set the Uuid from external');
 		}
 
-		return new self((int)$data['id'], $data['label']);
+		$instance = new self((int)$data['id'], $data['label']);
+		if (isset($data['uuid'])) {
+			$instance->setUuid($data['uuid']);
+		}
+		return $instance;
 	}
 
 	public function key(): int {
@@ -41,11 +49,20 @@ class SelectionOption implements \JsonSerializable {
 		return $this->label;
 	}
 
+	protected function setUuid(string $uuid): void {
+		$this->uuid = $uuid;
+	}
+
 	#[\Override]
 	public function jsonSerialize(): array {
-		return [
+		$out = [
 			'id' => $this->key,
 			'label' => $this->label,
 		];
+		/** @psalm-suppress RedundantPropertyInitializationCheck */
+		if (isset($this->uuid)) {
+			$out['uuid'] = $this->uuid;
+		}
+		return $out;
 	}
 }
