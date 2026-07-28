326b4cbbe32c4b75518de343daf6216c050e696c
Luka Trovic <luka@nextcloud.com>
2026-07-28 18:39:37 +0200

    feat: send import finished notification to View receipients
    Signed-off-by: Luka Trovic <luka@nextcloud.com>

--- lib/Activity/ActivityManager.php
+++ lib/Activity/ActivityManager.php
@@ -809,7 +809,8 @@ class ActivityManager {
 	}
 
 	public function findRecipientsByElement(Table|View $element): array {
-		$cacheKey = 'element_recipients_' . $element->getId();
+		$elementType = $element instanceof Table ? 'table' : 'view';
+		$cacheKey = 'element_recipients_' . $elementType . '_' . $element->getId();
 		$cached = $this->cache->get($cacheKey);
 		if ($cached !== null) {
 			return $cached;
@@ -822,13 +823,7 @@ class ActivityManager {
 			$recipients[] = $owner;
 		}
 
-		if ($element instanceof View) {
-			$recipients = array_merge($recipients, $this->shareService->findSharedWithUserIds($element->getId(), 'view'));
-		}
-		if ($element instanceof Table) {
-			$recipients = array_merge($recipients, $this->shareService->findSharedWithUserIds($element->getId(), 'table'));
-		}
-
+		$recipients = array_merge($recipients, $this->shareService->findSharedWithUserIds($element->getId(), $elementType));
 		$recipients = array_unique($recipients);
 		$this->cache->set($cacheKey, $recipients);
 		return $recipients;
