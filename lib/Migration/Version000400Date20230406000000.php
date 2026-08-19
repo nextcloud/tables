6a6622112e06147581a80901092dfefd313f9b16
Carl Schwan <carl@carlschwan.eu>
2026-08-19 08:05:49 +0200

    fix: typo in comment
    Co-authored-by: Enjeck <32180937+enjeck@users.noreply.github.com>
    Signed-off-by: Carl Schwan <carl@carlschwan.eu>

--- lib/Migration/Version000400Date20230406000000.php
+++ lib/Migration/Version000400Date20230406000000.php
@@ -37,17 +37,17 @@ class Version000400Date20230406000000 extends SimpleMigrationStep {
 
 			$column = $table->getColumn('selection_options');
 			$column->setLength(65535);
-			/** @psalm-suppress InvalidArgument Chane to ColumnType enum when app support nc >= 35 */
+			/** @psalm-suppress InvalidArgument Change to ColumnType enum when the app supports NC >= 35 */
 			$column->setType(Type::getType('text'));
 
 			$column = $table->getColumn('selection_default');
 			$column->setLength(65535);
-			/** @psalm-suppress InvalidArgument Chane to ColumnType enum when app support nc >= 35 */
+			/** @psalm-suppress InvalidArgument Change to ColumnType enum when the app supports NC >= 35 */
 			$column->setType(Type::getType('text'));
 
 			$column = $table->getColumn('text_default');
 			$column->setLength(65535);
-			/** @psalm-suppress InvalidArgument Chane to ColumnType enum when app support nc >= 35 */
+			/** @psalm-suppress InvalidArgument Change to ColumnType enum when the app supports NC >= 35 */
 			$column->setType(Type::getType('text'));
 		}
 
