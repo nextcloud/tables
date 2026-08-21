4e3905e9cd07f03a2f998078e0c4c8b55e22badd
Arthur Schiwon <blizzz@arthur-schiwon.de>
2026-08-21 21:10:29 +0200

    fix(Structure): wrong naming for filter
    Signed-off-by: Arthur Schiwon <blizzz@arthur-schiwon.de>

--- src/modules/modals/ImportContextSchemePreview.vue
+++ src/modules/modals/ImportContextSchemePreview.vue
@@ -1,6 +1,6 @@
 <!--
 	- SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
-	- SPDX-License-Identifier: AGPL-3.0-or-late
+	- SPDX-License-Identifier: AGPL-3.0-or-later
 -->
 <template>
 	<div class="import-table-scheme-preview">
